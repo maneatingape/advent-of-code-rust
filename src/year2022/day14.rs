06178352c023047c4c79b596d208c19ef8bde826
maneatingape <44142177+maneatingape@users.noreply.github.com>
2023-01-08 12:18:33 +0000

    Much faster approach using Vec instead of HashSet

--- src/year2022/day14.rs
+++ src/year2022/day14.rs
@@ -1,53 +1,54 @@
+use crate::util::collection::*;
 use crate::util::parse::to_vec;
 use crate::util::point::*;
-use std::collections::HashSet;
 
-pub fn parse(input: &str) -> HashSet<Point> {
-    let mut rock = HashSet::new();
+type Input = (Vec<u8>, i32, i32, i32);
 
-    for line in input.lines() {
-        to_vec::<i32>(line)
-            .windows(4)
-            .step_by(2)
-            .for_each(|window| {
-                if let [x1, y1, x2, y2] = window {
-                    helper(&mut rock, *x1, *y1, *x2, *y2);
+pub fn parse(input: &str) -> Input {
+    let points: Vec<Vec<i32>> = input.lines().map(to_vec::<i32>).collect();
+    let max_y = points.iter().flat_map(|row| row.iter().skip(1).step_by(2).max()).max().unwrap();
+
+    let width = 2 * max_y + 5;
+    let height = max_y + 3;
+    let start = max_y + 2;
+    let mut rock = Vec::tabulate((width * height) as usize, |_| 0u8);
+
+    for row in points {
+        for window in row.windows(4).step_by(2) {
+            if let [x1, y1, x2, y2] = window {
+                for x in *x1.min(x2)..=*x1.max(x2) {
+                    for y in *y1.min(y2)..=*y1.max(y2) {
+                        rock[((y * width) + (x - 500 + start)) as usize] = 1;
+                    }
                 }
-            });
-    }
-
-    fn helper(rock: &mut HashSet<Point>, x1: i32, y1: i32, x2: i32, y2: i32) {
-        for x in x1.min(x2)..=x1.max(x2) {
-            for y in y1.min(y2)..=y1.max(y2) {
-                rock.insert(Point(x, y));
             }
         }
     }
 
-    rock
+    (rock, width, height, start)
 }
 
-pub fn part1(input: &HashSet<Point>) -> usize {
+pub fn part1(input: &Input) -> usize {
     simulate(input, false)
 }
 
-pub fn part2(input: &HashSet<Point>) -> usize {
+pub fn part2(input: &Input) -> usize {
     simulate(input, true)
 }
 
-pub fn simulate(rock: &HashSet<Point>, floor: bool) -> usize {
-    fn helper(rock: &HashSet<Point>, sand: &mut HashSet<Point>, max_y: i32, unit: Point, floor: bool) -> bool {
-        if rock.contains(&unit) || sand.contains(&unit) {
+pub fn simulate(input: &Input, floor: bool) -> usize {
+    fn helper(sand: &mut Vec<u8>, width: i32, height: i32, floor: bool, unit: Point) -> bool {
+        if sand[(unit.1 * width + unit.0) as usize] != 0 {
             true
         }
-        else if unit.1 == max_y + 2 {
+        else if unit.1 == height - 1 {
             floor
         }
-        else if helper(rock, sand, max_y, Point(unit.0, unit.1 + 1), floor)
-            && helper(rock, sand, max_y, Point(unit.0 - 1, unit.1 + 1), floor)
-            && helper(rock, sand, max_y, Point(unit.0 + 1, unit.1 + 1), floor)
+        else if helper(sand, width, height, floor, Point(unit.0, unit.1 + 1))
+            && helper(sand, width, height, floor, Point(unit.0 - 1, unit.1 + 1))
+            && helper(sand, width, height, floor, Point(unit.0 + 1, unit.1 + 1))
         {
-            sand.insert(unit);
+            sand[(unit.1 * width + unit.0) as usize] = 2;
             true
         }
         else {
@@ -55,8 +56,8 @@ pub fn simulate(rock: &HashSet<Point>, floor: bool) -> usize {
         }
     }
 
-    let max_y = rock.iter().map(|p| p.1).max().unwrap();
-    let mut sand: HashSet<Point> = HashSet::new();
-    helper(rock, &mut sand, max_y, Point(500, 0), floor);
-    sand.len()
+    let (rock, width, height, start) = input;
+    let mut sand = rock.clone();
+    helper(&mut sand, *width, *height, floor, Point(*start, 0));
+    sand.iter().filter(|&&s| s == 2).count()
 }
