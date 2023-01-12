c50e02764b661ce8fd2c06ce44aa9c99ea0366cc
maneatingape <44142177+maneatingape@users.noreply.github.com>
2023-01-12 20:04:36 +0100

    Merge extension traits

--- src/util/collection.rs
+++ src/util/collection.rs
@@ -1,20 +1,15 @@
-pub trait VecOps1<T> {
-    fn fill(n: usize, t: T) -> Vec<T>;
+pub trait VecExt<T> {
+    fn fill(n: usize, t: T) -> Vec<T> where T: Copy;
+    fn tabulate(n: usize, f: impl Fn(usize) -> T) -> Vec<T>;
 }
 
-impl<T: Copy> VecOps1<T> for Vec<T> {
-    fn fill(n: usize, t: T) -> Vec<T> {
+impl<T> VecExt<T> for Vec<T> {
+    fn fill(n: usize, t: T) -> Vec<T> where T: Copy {
         let mut vec: Vec<T> = Vec::with_capacity(n);
         (0..n).for_each(|_| vec.push(t));
         vec
     }
-}
 
-pub trait VecOps2<T> {
-    fn tabulate(n: usize, f: impl Fn(usize) -> T) -> Vec<T>;
-}
-
-impl<T> VecOps2<T> for Vec<T> {
     fn tabulate(n: usize, f: impl Fn(usize) -> T) -> Vec<T> {
         let mut vec: Vec<T> = Vec::with_capacity(n);
         (0..n).for_each(|i| vec.push(f(i)));
