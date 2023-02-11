d8820c857e341133b0d5b95cd23042bf7f46ca69
maneatingape <44142177+maneatingape@users.noreply.github.com>
2023-02-11 09:06:06 +0100

    Clippy lint

--- src/util/parse.rs
+++ src/util/parse.rs
@@ -1,5 +1,3 @@
-#![allow(clippy::needless_lifetimes)]
-
 use std::iter::{Filter, Map};
 use std::str::{FromStr, Split};
 
@@ -18,11 +16,11 @@ impl Unsigned for u64 {}
 impl Unsigned for usize {}
 
 pub trait ParseUnsigned {
-    fn iter_unsigned<'a, T: Unsigned>(&'a self) -> Wrapper<'a, T>;
+    fn iter_unsigned<T: Unsigned>(&self) -> Wrapper<'_, T>;
 }
 
 impl ParseUnsigned for &str {
-    fn iter_unsigned<'a, T: Unsigned>(&'a self) -> Wrapper<'a, T> {
+    fn iter_unsigned<T: Unsigned>(&self) -> Wrapper<'_, T> {
         let not_numeric: fn(char) -> bool = |c| !c.is_ascii_digit();
         let not_empty: fn(&&str) -> bool = |s| !s.is_empty();
         self.split(not_numeric).filter(not_empty).map(from)
@@ -34,11 +32,11 @@ impl Signed for i32 {}
 impl Signed for i64 {}
 
 pub trait ParseSigned {
-    fn iter_signed<'a, T: Signed>(&'a self) -> Wrapper<'a, T>;
+    fn iter_signed<T: Signed>(&self) -> Wrapper<'_, T>;
 }
 
 impl ParseSigned for &str {
-    fn iter_signed<'a, T: Signed>(&'a self) -> Wrapper<'a, T> {
+    fn iter_signed<T: Signed>(&self) -> Wrapper<'_, T> {
         let not_numeric: fn(char) -> bool = |c| !c.is_ascii_digit() && c != '-';
         let not_empty: fn(&&str) -> bool = |s| !s.is_empty();
         self.split(not_numeric).filter(not_empty).map(from)
