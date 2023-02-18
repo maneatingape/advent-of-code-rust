a86069688023861573a6cb7e3b09fdfc3ab463ce
maneatingape <44142177+maneatingape@users.noreply.github.com>
2023-02-19 00:23:56 +0100

    Add FastHashMap

--- src/util/hash.rs
+++ src/util/hash.rs
@@ -5,9 +5,10 @@ use std::collections::{HashMap, HashSet};
 use std::hash::{BuildHasher, Hasher};
 
 pub type FastSet<T> = HashSet<T, BuildFxHasher>;
-pub type FastMap<T> = HashMap<T, BuildFxHasher>;
+pub type FastMap<K, V> = HashMap<K, V, BuildFxHasher>;
 
 pub struct FastSetBuilder;
+pub struct FastMapBuilder;
 
 impl FastSetBuilder {
     pub fn empty<T>() -> FastSet<T> {
@@ -19,6 +20,16 @@ impl FastSetBuilder {
     }
 }
 
+impl FastMapBuilder {
+    pub fn empty<K, V>() -> FastMap<K, V> {
+        HashMap::with_hasher(BuildFxHasher)
+    }
+
+    pub fn with_capacity<K, V>(capacity: usize) -> FastMap<K, V> {
+        HashMap::with_capacity_and_hasher(capacity, BuildFxHasher)
+    }
+}
+
 #[derive(Clone, Copy, Default)]
 pub struct BuildFxHasher;
 
@@ -26,18 +37,18 @@ impl BuildHasher for BuildFxHasher {
     type Hasher = FxHasher;
 
     fn build_hasher(&self) -> Self::Hasher {
-        FxHasher { hash: 0 }
+        FxHasher { state: 0 }
     }
 }
 
 pub struct FxHasher {
-    hash: u64,
+    state: u64,
 }
 
 impl FxHasher {
     #[inline]
-    fn add_to_hash(&mut self, i: u64) {
-        self.hash = (self.hash.rotate_left(5) ^ i).wrapping_mul(0x517cc1b727220a95);
+    fn hash(&mut self, i: u64) {
+        self.state = (self.state.rotate_left(5) ^ i).wrapping_mul(0x517cc1b727220a95);
     }
 }
 
@@ -45,49 +56,49 @@ impl Hasher for FxHasher {
     #[inline]
     fn write(&mut self, mut bytes: &[u8]) {
         while bytes.len() >= 8 {
-            self.add_to_hash(u64::from_ne_bytes(bytes[..8].try_into().unwrap()));
+            self.hash(u64::from_ne_bytes(bytes[..8].try_into().unwrap()));
             bytes = &bytes[8..];
         }
         if bytes.len() >= 4 {
-            self.add_to_hash(u32::from_ne_bytes(bytes[..4].try_into().unwrap()) as u64);
+            self.hash(u32::from_ne_bytes(bytes[..4].try_into().unwrap()) as u64);
             bytes = &bytes[4..];
         }
         if bytes.len() >= 2 {
-            self.add_to_hash(u16::from_ne_bytes(bytes[..2].try_into().unwrap()) as u64);
+            self.hash(u16::from_ne_bytes(bytes[..2].try_into().unwrap()) as u64);
             bytes = &bytes[2..];
         }
         if !bytes.is_empty() {
-            self.add_to_hash(bytes[0] as u64);
+            self.hash(bytes[0] as u64);
         }
     }
 
     #[inline]
     fn write_u8(&mut self, i: u8) {
-        self.add_to_hash(i as u64);
+        self.hash(i as u64);
     }
 
     #[inline]
     fn write_u16(&mut self, i: u16) {
-        self.add_to_hash(i as u64);
+        self.hash(i as u64);
     }
 
     #[inline]
     fn write_u32(&mut self, i: u32) {
-        self.add_to_hash(i as u64);
+        self.hash(i as u64);
     }
 
     #[inline]
     fn write_u64(&mut self, i: u64) {
-        self.add_to_hash(i);
+        self.hash(i);
     }
 
     #[inline]
     fn write_usize(&mut self, i: usize) {
-        self.add_to_hash(i as u64);
+        self.hash(i as u64);
     }
 
     #[inline]
     fn finish(&self) -> u64 {
-        self.hash
+        self.state
     }
 }
