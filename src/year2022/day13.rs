bb0f8f7699f55b3a9701683ff7fe44d3f1663d35
maneatingape <44142177+maneatingape@users.noreply.github.com>
2023-02-25 19:13:19 +0100

    Document day 13

--- src/year2022/day13.rs
+++ src/year2022/day13.rs
@@ -1,3 +1,25 @@
+//! # Distress Signal
+//!
+//! One possible approach is to parse the input into a tree, then compare recursively node
+//! by node. We're going to use a much faster and simpler approach by noting an observation about
+//! the input data. If the sequence `10` is replaced by any single character greater than `9` then
+//! we can compare the 2 packets *lexigraphically*. We'll replace all occurences of `10` with `A`
+//! then compare packets character by character.
+//!
+//! The rules to compare 2 packets become:
+//! * If both characters are the same then it's a draw, move onto the next character in each packet.
+//! * If the first packet is `]` and the second packet anything else, then the first list is shorter
+//!   so the packets are in order.
+//! * Conversely if the second packet is `]` and the first packet anything else, the packets are not
+//!   in order.
+//! * If the first packet is an opening `[` and the second character anything else, then we're
+//!   comparing a number with a list, so *push* the second character back onto the list to check
+//!   again along with a closing `]` character.
+//! * Do a similar push if the second character is an opening `[` and the first anything else.
+//! * Finally compare the 2 characters by value. Since we've already covered the equal case, one
+//!   is guaranteed to be greater or less than the other.
+
+/// Replace `10` with `A` in each packet and strip empty lines.
 pub fn parse(input: &str) -> Vec<String> {
     input
         .lines()
@@ -6,6 +28,7 @@ pub fn parse(input: &str) -> Vec<String> {
         .collect()
 }
 
+/// Count adjacent pairs of packets that are in order.
 pub fn part1(input: &[String]) -> usize {
     input
         .chunks_exact(2)
@@ -17,6 +40,19 @@ pub fn part1(input: &[String]) -> usize {
         .sum()
 }
 
+/// Find the position of `[[2]]` and `[[6]]` in linear `O(n)` time.
+///
+/// One approach would be to insert `[[2]]` and `[[6]]` into the list, sort in `O(nlogn)` time,
+/// then find the indices of the 2 values in `O(n)` time.
+///
+/// A much faster approach is to iterate over the list, comparing each packet first with `[[2]]`.
+/// If the packets are in order, then increment the positions of *both* `[[2]]` and `[[6]]`,
+/// since `[[2]]` is less than `[[6]]`.
+///
+/// If the packet and `[[2]]` are not in order, then also check against `[[6]]`, incrementing only
+/// the second index if the 2 packets are in order.
+///
+/// This obtains the relative indices of `[[2]]` and `[[6]]` efficiently in fewer than `2n` comparisons.
 pub fn part2(input: &[String]) -> u32 {
     let mut first = 1;
     let mut second = 2;
@@ -33,6 +69,14 @@ pub fn part2(input: &[String]) -> u32 {
     first * second
 }
 
+/// Compare 2 packets using the rules listed in the module description.
+///
+/// It's faster to use 2 temporary `vec`s to store extra characters, rather than copy each
+/// packet into a mutable [`VecDeque`]. We use the [`or_else`] method on [`Option`] to check
+/// in the temporary `vec` for available characters first.
+///
+/// [`VecDeque`]: std::collections::VecDeque
+/// [`or_else`]: Option::or_else
 fn compare(left: &str, right: &str) -> bool {
     let mut left_iter = left.chars();
     let mut right_iter = right.chars();
