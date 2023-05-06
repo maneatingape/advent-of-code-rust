d0eea0ed0655d35ce21782a34feed12b729b8282
maneatingape <44142177+maneatingape@users.noreply.github.com>
2023-05-06 23:08:43 +0200

    Faster in-place conversion of integer to string

--- src/year2015/day04.rs
+++ src/year2015/day04.rs
@@ -1,29 +1,44 @@
 use crate::util::md5::hash;
-use std::fmt::Write;
 
-pub fn parse(input: &str) -> &str {
-    input.trim()
+pub fn parse(input: &str) -> &[u8] {
+    input.trim().as_bytes()
 }
 
-pub fn part1(input: &str) -> u32 {
+pub fn part1(input: &[u8]) -> u32 {
     find(input, 0xfffff000)
 }
 
-pub fn part2(input: &str) -> u32 {
+pub fn part2(input: &[u8]) -> u32 {
     find(input, 0xffffff00)
 }
 
-fn find(input: &str, mask: u32) -> u32 {
-    let mut index = 0;
-    let mut s = String::with_capacity(32);
-    let mut a = 0x12345678;
+fn find(input: &[u8], mask: u32) -> u32 {
+    let mut count = 0;
+    let mut size = input.len();
+    let mut buffer = [b'0'; 32];
 
-    while (a & mask) != 0 {
-        index += 1;
-        s.clear();
-        write!(&mut s, "{input}{index}").expect("Not enough space");
-        a = hash(s.as_bytes()).0;
+    buffer[..size].copy_from_slice(input);
+
+    loop {
+        count += 1;
+
+        let mut index = size;
+        loop {
+            if buffer[index] < b'9' {
+                buffer[index] += 1;
+                break;
+            } else if index == input.len() {
+                buffer[index] = b'1';
+                size += 1;
+                break;
+            } else {
+                buffer[index] = b'0';
+                index -= 1;
+            }
+        }
+
+        if hash(&buffer[..(size + 1)]).0 & mask == 0 {
+            return count;
+        }
     }
-
-    index
 }
