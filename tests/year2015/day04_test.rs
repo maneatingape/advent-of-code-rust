b8edbb27e1cb3524aa5118585538d40745bd1d2b
maneatingape <44142177+maneatingape@users.noreply.github.com>
2023-05-07 00:31:17 +0200

    Found prefix that has low values for both parts to enable testing in reasonable time

--- tests/year2015/day04_test.rs
+++ tests/year2015/day04_test.rs
@@ -1,1 +1,15 @@
-// No tests possible in reasonable time
+use aoc::year2015::day04::*;
+
+const EXAMPLE: &str = "cxsaadws";
+
+#[test]
+fn part1_test() {
+    let input = parse(EXAMPLE);
+    assert_eq!(part1(&input), 218);
+}
+
+#[test]
+fn part2_test() {
+    let input = parse(EXAMPLE);
+    assert_eq!(part2(&input), 455);
+}
