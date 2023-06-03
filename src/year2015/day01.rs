7d6deab19f1f3fe2a24822a18f74ca8f2495ad2d
maneatingape <44142177+maneatingape@users.noreply.github.com>
2023-06-03 09:30:29 +0200

    Document year 2015 day 1-2

--- src/year2015/day01.rs
+++ src/year2015/day01.rs
@@ -1,3 +1,8 @@
+//! # Not Quite Lisp
+//!
+//! The input is first converted into bytes. This is safe as it contains only ASCII characters.
+//! Then each parenthesis is parsed into either +1 or -1, treating the trailing newline
+//! as a special case of 0.
 pub fn parse(input: &str) -> Vec<i32> {
     fn helper(b: &u8) -> i32 {
         match b {
--- src/year2015/day02.rs
+++ src/year2015/day02.rs
@@ -1,3 +1,13 @@
+//! # I Was Told There Would Be No Math
+//!
+//! To extract the numbers when parsing the input we use our utility [`iter_unsigned`] and [`chunk`]
+//! functions.
+//!
+//! Sorting the dimensions in ascending order makes calculating the smallest side or smallest
+//! perimeter straightforward.
+//!
+//! [`iter_unsigned`]: crate::util::parse
+//! [`chunk`]: crate::util::iter
 use crate::util::iter::*;
 use crate::util::parse::*;
 
