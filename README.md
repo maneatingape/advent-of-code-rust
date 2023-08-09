9bdf1db0d4f1e5481a0889e11388cad2b8d2a7f7
maneatingape <44142177+maneatingape@users.noreply.github.com>
2023-08-09 22:07:41 +0200

    Much faster specialized approach

--- README.md
+++ README.md
@@ -148,7 +148,7 @@ pie
     title Year 2020 benchmark time by % of total
     "Day 15" : 160000
     "Day 23" : 110000
-    "Others" : 16759
+    "Others" : 16676
 ```
 
 | Day | Problem | Solution | Benchmark (μs) |
@@ -170,7 +170,7 @@ pie
 | 15 | [Rambunctious Recitation](https://adventofcode.com/2020/day/15) | [Source](src/year2020/day15.rs) | 160000 |
 | 16 | [Ticket Translation](https://adventofcode.com/2020/day/16) | [Source](src/year2020/day16.rs) | 102 |
 | 17 | [Conway Cubes](https://adventofcode.com/2020/day/17) | [Source](src/year2020/day17.rs) | 414 |
-| 18 | [Operation Order](https://adventofcode.com/2020/day/18) | [Source](src/year2020/day18.rs) | 109 |
+| 18 | [Operation Order](https://adventofcode.com/2020/day/18) | [Source](src/year2020/day18.rs) | 26 |
 | 19 | [Monster Messages](https://adventofcode.com/2020/day/19) | [Source](src/year2020/day19.rs) | 384 |
 | 20 | [Jurassic Jigsaw](https://adventofcode.com/2020/day/20) | [Source](src/year2020/day20.rs) | 43 |
 | 21 | [Allergen Assessment](https://adventofcode.com/2020/day/21) | [Source](src/year2020/day21.rs) | 51 |
--- src/year2020/day18.rs
+++ src/year2020/day18.rs
@@ -1,89 +1,77 @@
 //! # Operation Order
 //!
-//! We use the [shunting yard algorithm](https://en.wikipedia.org/wiki/Shunting_yard_algorithm)
-//! to convert each expressions from infix to [postfix](https://en.wikipedia.org/wiki/Reverse_Polish_notation).
+//! For part one the operator precedence is the same so we proceed from left to right for
+//! each expression. Parentheses are handled via recursion, so we return either when encountering
+//! the end of the string or a `)` character.
 //!
-//! Postfix expressions are straightforward to evaluate using a stack (and there's even
-//! an [entire language](https://en.wikipedia.org/wiki/Forth_(programming_language))
-//! designed around them).
-#[derive(PartialEq, Eq)]
-enum Token {
-    Number(u64),
-    Add,
-    Mul,
-    Open,
+//! For part two whenever we encounter the lower priority `*` operator then we *implicitly* insert
+//! parentheses around the remaining expression. For example:
+//!
+//! `1 * 2 * 3 * 4 => 1 * (2 * (3 * (4)))`
+//! `1 + 2 * 3 + 4 => 1 + 2 * (3 + 4)`
+//! `1 + (2 * 3 * 4) + 5 => 1 + (2 * (3 * (4))) + 5`
+use std::str::Bytes;
+
+pub fn parse(input: &str) -> Vec<&str> {
+    input.lines().collect()
 }
 
-pub fn parse(input: &str) -> Vec<&[u8]> {
-    input.lines().map(str::as_bytes).collect()
-}
+pub fn part1(input: &[&str]) -> u64 {
+    fn helper(bytes: &mut Bytes) -> u64 {
+        let mut total = value(bytes, helper);
 
-pub fn part1(input: &[&[u8]]) -> u64 {
-    eval(input, true)
-}
-pub fn part2(input: &[&[u8]]) -> u64 {
-    eval(input, false)
-}
-
-fn eval(input: &[&[u8]], part_one: bool) -> u64 {
-    let mut output = Vec::new();
-    let mut operator = Vec::new();
-
-    let mut push = |token| match token {
-        Token::Number(n) => output.push(n),
-        Token::Add => {
-            let a = output.pop().unwrap();
-            let b = output.pop().unwrap();
-            output.push(a + b);
-        }
-        Token::Mul => {
-            let a = output.pop().unwrap();
-            let b = output.pop().unwrap();
-            output.push(a * b);
-        }
-        Token::Open => unreachable!(),
-    };
-
-    for &line in input {
-        for &c in line {
-            match c {
-                b' ' => (),
-                b'(' => operator.push(Token::Open),
-                b')' => loop {
-                    match operator.pop().unwrap() {
-                        Token::Open => break,
-                        next => push(next),
-                    }
-                },
-                b'+' => {
-                    loop {
-                        match operator.last() {
-                            Some(Token::Add) => push(operator.pop().unwrap()),
-                            Some(Token::Mul) if part_one => push(operator.pop().unwrap()),
-                            _ => break,
-                        }
-                    }
-                    operator.push(Token::Add);
-                }
-                b'*' => {
-                    while let Some(Token::Add | Token::Mul) = operator.last() {
-                        push(operator.pop().unwrap());
-                    }
-                    operator.push(Token::Mul);
-                }
-                n if n.is_ascii_digit() => {
-                    let n = (n - b'0') as u64;
-                    push(Token::Number(n));
-                }
-                _ => unreachable!(),
+        while let Some(operation) = next(bytes) {
+            let value = value(bytes, helper);
+            if operation == b'+' {
+                total += value;
+            } else {
+                total *= value;
             }
         }
 
-        // Drains remaining operators to the output.
-        while let Some(token) = operator.pop() {
-            push(token);
-        }
+        total
     }
 
-    output.iter().sum()
+    input.iter().map(|line| helper(&mut line.bytes())).sum()
+}
+
+pub fn part2(input: &[&str]) -> u64 {
+    fn helper(bytes: &mut Bytes) -> u64 {
+        let mut total = value(bytes, helper);
+
+        while let Some(operation) = next(bytes) {
+            if operation == b'+' {
+                total += value(bytes, helper);
+            } else {
+                // Implicitly insert '(' and ')' around the remaining sub-expression so when it
+                // finishes we break too.
+                total *= helper(bytes);
+                break;
+            }
+        }
+
+        total
+    }
+
+    input.iter().map(|line| helper(&mut line.bytes())).sum()
+}
+
+/// Convenience wrapper around [`Bytes`] iterator. Encountering a `)` is also considered end of
+/// sequence. The expressions are consistently formatted so encountering a space just means
+/// skip and return the next character, which will always be present.
+fn next(bytes: &mut Bytes) -> Option<u8> {
+    match bytes.next() {
+        None | Some(b')') => None,
+        Some(b' ') => bytes.next(),
+        other => other,
+    }
+}
+
+/// Convenience wrapper to return the value of either the next raw digit literal or a
+/// sub-expression nested in parentheses.
+fn value(bytes: &mut Bytes, helper: impl Fn(&mut Bytes) -> u64) -> u64 {
+    match next(bytes).unwrap() {
+        b'(' => helper(bytes),
+        b => (b - b'0') as u64,
+    }
 }
