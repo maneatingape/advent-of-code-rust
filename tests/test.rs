683ca898c3edc0a686316d26a155983ecd593c55
maneatingape <44142177+maneatingape@users.noreply.github.com>
2023-09-03 22:52:30 +0200

    Add empty template for new day

--- tests/test.rs
+++ tests/test.rs
@@ -1,3 +1,33 @@
+// Templates
+
+// pub fn parse(_input: &str) -> Vec<u32> {
+//     vec![]
+// }
+//
+// pub fn part1(_input: &[u32]) -> u32 {
+//     123
+// }
+//
+// pub fn part2(_input: &[u32]) -> u32 {
+//     456
+// }
+
+// use aoc::year2023::day00::*;
+//
+// const EXAMPLE: &str = "";
+//
+// #[test]
+// fn part1_test() {
+//     let input = parse(EXAMPLE);
+//     assert_eq!(part1(&input), 123);
+// }
+//
+// #[test]
+// fn part2_test() {
+//     let input = parse(EXAMPLE);
+//     assert_eq!(part2(&input), 456);
+// }
+
 mod year2015 {
     mod day01_test;
     mod day02_test;
