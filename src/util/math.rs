165a7c2cad2742c9ccb673cd9a96373229d7c72b
maneatingape <44142177+maneatingape@users.noreply.github.com>
2023-09-04 19:42:18 +0200

    Add lcm method and update doc

--- src/util/math.rs
+++ src/util/math.rs
@@ -1,19 +1,28 @@
 //! Extended mathematical operations.
 //!
-//! * Greatest common divisor
-//! * Modular exponentation
-//! * Modular inverse
+//! * [Greatest common divisor](https://en.wikipedia.org/wiki/Greatest_common_divisor)
+//!   of 2 numbers using the
+//!   [Euclidean algorithm](https://en.wikipedia.org/wiki/Euclidean_algorithm).
+//!
+//! * [Least common multiple](https://en.wikipedia.org/wiki/Least_common_multiple)
+//!
+//! * [Modular exponentation](https://en.wikipedia.org/wiki/Modular_exponentiation).
+//!   Calculates bᵉ mod m efficiently using
+//!   [exponentiation by squaring](https://en.wikipedia.org/wiki/Exponentiation_by_squaring).
+
+//! * [Modular multiplicative inverse](https://en.wikipedia.org/wiki/Modular_multiplicative_inverse)
+//!   calculated using the [extended Euclidean algorithm](https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm).
 use crate::util::integer::*;
 
 pub trait MathOps<T: Integer<T>> {
     fn gcd(self, b: T) -> T;
+    fn lcm(self, b: T) -> T;
     fn mod_pow(self, e: T, m: T) -> T;
     fn mod_inv(self, m: T) -> T;
 }
 
 impl<T: Integer<T>> MathOps<T> for T {
-    /// Greatest common divisor of 2 numbers using the
-    /// [Euclidean algorithm](https://en.wikipedia.org/wiki/Euclidean_algorithm).
+    /// Greatest common divisor
     fn gcd(self, mut b: T) -> T {
         let mut a = self;
 
@@ -24,8 +33,12 @@ impl<T: Integer<T>> MathOps<T> for T {
         a
     }
 
-    /// Calculates bᵉ mod m efficiently using
-    /// [exponentiation by squaring](https://en.wikipedia.org/wiki/Exponentiation_by_squaring).
+    // Least common multiple
+    fn lcm(self, b: T) -> T {
+        self * (b / self.gcd(b))
+    }
+
+    // Modular exponentation
     fn mod_pow(self, mut e: T, m: T) -> T {
         let mut b = self;
         let mut c = T::ONE;
@@ -41,8 +54,7 @@ impl<T: Integer<T>> MathOps<T> for T {
         c
     }
 
-    /// [Modular multiplicative inverse](https://en.wikipedia.org/wiki/Modular_multiplicative_inverse)
-    /// calculated using the [extended Euclidean algorithm](https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm).
+    // Modular multiplicative inverse
     fn mod_inv(self, m: T) -> T {
         let mut t = T::ZERO;
         let mut newt = T::ONE;
