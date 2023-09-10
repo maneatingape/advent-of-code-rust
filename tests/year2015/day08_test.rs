24f87c1b6ce21822f7d83647b75c232efbda6434
maneatingape <44142177+maneatingape@users.noreply.github.com>
2023-09-10 13:22:56 +0200

    Expand Clippy lints to cover tests and conditional features

--- tests/year2015/day08_test.rs
+++ tests/year2015/day08_test.rs
@@ -9,11 +9,11 @@ const EXAMPLE: &str = r#"
 #[test]
 fn part1_test() {
     let input = parse(EXAMPLE);
-    assert_eq!(part1(&input), 12);
+    assert_eq!(part1(input), 12);
 }
 
 #[test]
 fn part2_test() {
     let input = parse(EXAMPLE);
-    assert_eq!(part2(&input), 19);
+    assert_eq!(part2(input), 19);
 }
--- tests/year2022/day17_test.rs
+++ tests/year2022/day17_test.rs
@@ -5,11 +5,11 @@ const EXAMPLE: &str = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
 #[test]
 fn part1_test() {
     let input = parse(EXAMPLE);
-    assert_eq!(part1(&input), 3068);
+    assert_eq!(part1(input), 3068);
 }
 
 #[test]
 fn part2_test() {
     let input = parse(EXAMPLE);
-    assert_eq!(part2(&input), 1514285714288);
+    assert_eq!(part2(input), 1514285714288);
 }
