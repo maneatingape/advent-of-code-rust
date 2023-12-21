82ad71bfd9a981570f4672cb6a19fe9a9242c610
maneatingape <44142177+maneatingape@users.noreply.github.com>
2023-12-21 22:04:22 +0100

    Faster cleaner approach creating a graph from bricks

--- src/year2023/day22.rs
+++ src/year2023/day22.rs
@@ -1,114 +1,100 @@
-use crate::util::hash::*;
+#![allow(clippy::needless_range_loop)]
+
 use crate::util::iter::*;
 use crate::util::parse::*;
+use std::collections::VecDeque;
 
-type Point3D = (u32, u32, u32);
-type Brick = (Vec<Point3D>, Vec<Point3D>);
-type Input = (usize, usize);
+pub struct Input {
+    up: Vec<Vec<usize>>,
+    down: Vec<Vec<usize>>,
+}
 
 pub fn parse(input: &str) -> Input {
-    let mut bricks = Vec::new();
+    let mut bricks: Vec<_> = input.iter_unsigned::<usize>().chunk::<6>().collect();
+    let mut heights = [[0; 10]; 10];
+    let mut indices = [[usize::MAX; 10]; 10];
+    let mut up = vec![Vec::new(); bricks.len()];
+    let mut down = vec![Vec::new(); bricks.len()];
 
-    for [x1, y1, z1, x2, y2, z2] in input.iter_unsigned::<u32>().chunk::<6>() {
-        let mut cubes = Vec::new();
-        let mut shadow = Vec::new();
+    // Sort ascending by lowest z coordinate.
+    bricks.sort_unstable_by_key(|b| b[2]);
 
-        if x2 > x1 {
-            for x in x1..=x2 {
-                cubes.push((x, y1, z1));
-                shadow.push((x, y1, z1));
-            }
-        } else if y2 > y1 {
+    for (i, &[x1, y1, z1, x2, y2, z2]) in bricks.iter().enumerate() {
+        let height = z2 - z1 + 1;
+        let mut top = 0;
+        let mut previous = usize::MAX;
+
+        for x in x1..=x2 {
             for y in y1..=y2 {
-                cubes.push((x1, y, z1));
-                shadow.push((x1, y, z1));
+                top = top.max(heights[x][y]);
             }
-        } else if z2 > z1 {
-            for z in z1..=z2 {
-                cubes.push((x1, y1, z));
-            }
-            shadow.push((x1, y1, z1));
-        } else {
-            cubes.push((x1, y1, z1));
-            shadow.push((x1, y1, z1));
         }
 
-        bricks.push((cubes, shadow));
-    }
+        for x in x1..=x2 {
+            for y in y1..=y2 {
+                if heights[x][y] == top {
+                    let index = indices[x][y];
+                    if index != previous {
+                        up[index].push(i);
+                        down[i].push(index);
+                        previous = index;
+                    }
+                }
 
-    let (settled, _) = fall(&bricks);
-    let mut part_one = 0;
-    let mut part_two = 0;
-
-    for i in 0..settled.len() {
-        let mut first = settled.clone();
-        first.remove(i);
-
-        let (_, moved) = fall(&first);
-
-        if moved == 0 {
-            part_one += 1;
+                heights[x][y] = top + height;
+                indices[x][y] = i;
+            }
         }
-        part_two += moved;
     }
 
-    (part_one, part_two)
+    Input { up, down }
 }
 
 pub fn part1(input: &Input) -> usize {
-    input.0
+    let Input { down, .. } = input;
+    let mut safe = vec![true; down.len()];
+
+    for underneath in down {
+        if underneath.len() == 1 {
+            safe[underneath[0]] = false;
+        }
+    }
+
+    safe.iter().filter(|&&b| b).count()
 }
 
 pub fn part2(input: &Input) -> usize {
-    input.1
-}
+    let Input { up, down } = input;
+    let mut safe = vec![true; down.len()];
 
-fn fall(bricks: &[Brick]) -> (Vec<Brick>, usize) {
-    let mut bricks = bricks.to_vec();
-    let mut space = FastSet::new();
-
-    for brick in &bricks {
-        for &point in &brick.0 {
-            space.insert(point);
+    for underneath in down {
+        if underneath.len() == 1 {
+            safe[underneath[0]] = false;
         }
     }
 
-    let mut change = true;
-    let mut moved = vec![false; bricks.len()];
+    let mut result = 0;
+    let mut todo = VecDeque::new();
+    let mut removed = vec![usize::MAX; down.len()];
 
-    while change {
-        change = false;
+    for (start, &safe) in safe.iter().enumerate() {
+        if safe {
+            continue;
+        }
 
-        for i in 0..bricks.len() {
-            let brick = &mut bricks[i];
-            let next = down(&brick.0, &brick.1);
+        todo.push_back(start);
+        removed[start] = start;
 
-            if next.1.iter().any(|point| space.contains(point)) {
-                continue;
+        while let Some(current) = todo.pop_front() {
+            for &next in &up[current] {
+                if removed[next] != start && down[next].iter().all(|&i| removed[i] == start) {
+                    result += 1;
+                    removed[next] = start;
+                    todo.push_back(next);
+                }
             }
-            if next.1.iter().any(|&(_, _, z)| z == 0) {
-                continue;
-            }
-
-            for point in &brick.0 {
-                space.remove(point);
-            }
-            for &point in &next.0 {
-                space.insert(point);
-            }
-
-            change = true;
-            moved[i] = true;
-            *brick = next;
         }
     }
 
-    let sum = moved.iter().filter(|&&b| b).count();
-    (bricks, sum)
-}
-
-fn down(cubes: &[Point3D], shadow: &[Point3D]) -> (Vec<Point3D>, Vec<Point3D>) {
-    let first = cubes.iter().map(|&(x, y, z)| (x, y, z - 1)).collect();
-    let second = shadow.iter().map(|&(x, y, z)| (x, y, z - 1)).collect();
-    (first, second)
+    result
 }
