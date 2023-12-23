f7916edd1af1d5c9cf73492f22c234811eb546a5
maneatingape <44142177+maneatingape@users.noreply.github.com>
2023-12-23 12:07:02 +0000

    Much faster approach using smaller state of amount of accumulated round rocks per fixed rock

--- README.md
+++ README.md
@@ -94,7 +94,7 @@ pie
 | 11 | [Cosmic Expansion](https://adventofcode.com/2023/day/11) | [Source](src/year2023/day11.rs) | 12 |
 | 12 | [Hot Springs](https://adventofcode.com/2023/day/12) | [Source](src/year2023/day12.rs) | 1150 |
 | 13 | [Point of Incidence](https://adventofcode.com/2023/day/13) | [Source](src/year2023/day13.rs) | 64 |
-| 14 | [Parabolic Reflector Dish](https://adventofcode.com/2023/day/14) | [Source](src/year2023/day14.rs) | 5635 |
+| 14 | [Parabolic Reflector Dish](https://adventofcode.com/2023/day/14) | [Source](src/year2023/day14.rs) | 661 |
 | 15 | [Lens Library](https://adventofcode.com/2023/day/15) | [Source](src/year2023/day15.rs) | 83 |
 | 16 | [The Floor Will Be Lava](https://adventofcode.com/2023/day/16) | [Source](src/year2023/day16.rs) | 831 |
 | 17 | [Clumsy Crucible](https://adventofcode.com/2023/day/17) | [Source](src/year2023/day17.rs) | 2566 |
--- src/year2023/day14.rs
+++ src/year2023/day14.rs
@@ -7,137 +7,178 @@ use crate::util::grid::*;
 use crate::util::hash::*;
 use crate::util::point::*;
 
-pub fn parse(input: &str) -> Grid<u8> {
-    Grid::parse(input)
+pub struct Input {
+    width: i32,
+    height: i32,
+    rounded: Vec<i16>,
+    fixed_north: Vec<i16>,
+    fixed_west: Vec<i16>,
+    fixed_south: Vec<i16>,
+    fixed_east: Vec<i16>,
+    roll_north: Vec<i16>,
+    roll_west: Vec<i16>,
+    roll_south: Vec<i16>,
+    roll_east: Vec<i16>,
 }
 
-pub fn part1(input: &Grid<u8>) -> i32 {
-    let grid = &mut input.clone();
-    north(grid);
-    load(grid)
+pub fn parse(input: &str) -> Input {
+    let inner = Grid::parse(input);
+    let mut grid = Grid {
+        width: inner.width + 2,
+        height: inner.height + 2,
+        bytes: vec![b'#'; ((inner.width + 2) * (inner.height + 2)) as usize],
+    };
+
+    // Copy
+    for y in 0..inner.width {
+        for x in 0..inner.width {
+            let src = Point::new(x, y);
+            let dst = Point::new(x + 1, y + 1);
+            grid[dst] = inner[src];
+        }
+    }
+
+    let copy = || Grid { width: grid.width, height: grid.height, bytes: vec![0; grid.bytes.len()] };
+
+    let mut rounded = Vec::new();
+    let mut north = copy();
+    let mut west = copy();
+    let mut south = copy();
+    let mut east = copy();
+    let mut roll_north = Vec::new();
+    let mut roll_west = Vec::new();
+    let mut roll_south = Vec::new();
+    let mut roll_east = Vec::new();
+
+    // Rounded
+    for y in 0..grid.height {
+        for x in 0..grid.width {
+            let point = Point::new(x, y);
+            if grid[point] == b'O' {
+                rounded.push((grid.width * point.y + point.x) as i16);
+            }
+        }
+    }
+
+    // North
+    for x in 0..grid.width {
+        for y in 0..grid.height {
+            let point = Point::new(x, y);
+            if grid[point] == b'#' {
+                roll_north.push((grid.width * point.y + point.x) as i16);
+            }
+            north[point] = (roll_north.len() - 1) as i16;
+        }
+    }
+
+    // West
+    for y in 0..grid.height {
+        for x in 0..grid.width {
+            let point = Point::new(x, y);
+            if grid[point] == b'#' {
+                roll_west.push((grid.width * point.y + point.x) as i16);
+            }
+            west[point] = (roll_west.len() - 1) as i16;
+        }
+    }
+
+    // South
+    for x in 0..grid.width {
+        for y in (0..grid.height).rev() {
+            let point = Point::new(x, y);
+            if grid[point] == b'#' {
+                roll_south.push((grid.width * point.y + point.x) as i16);
+            }
+            south[point] = (roll_south.len() - 1) as i16;
+        }
+    }
+
+    // East
+    for y in 0..grid.height {
+        for x in (0..grid.width).rev() {
+            let point = Point::new(x, y);
+            if grid[point] == b'#' {
+                roll_east.push((grid.width * point.y + point.x) as i16);
+            }
+            east[point] = (roll_east.len() - 1) as i16;
+        }
+    }
+
+    Input {
+        width: grid.width,
+        height: grid.height,
+        rounded,
+        fixed_north: north.bytes,
+        fixed_west: west.bytes,
+        fixed_south: south.bytes,
+        fixed_east: east.bytes,
+        roll_north,
+        roll_west,
+        roll_south,
+        roll_east,
+    }
 }
 
-pub fn part2(input: &Grid<u8>) -> i32 {
-    let grid = &mut input.clone();
+pub fn part1(input: &Input) -> i32 {
+    let Input { width, height, fixed_north, roll_north, .. } = input;
 
+    let mut result = 0;
+    let rounded = &mut input.rounded.clone();
+    let state = tilt(rounded, fixed_north, roll_north, *width as i16);
+
+    for (&a, &b) in input.roll_north.iter().zip(state.iter()) {
+        for index in (a..b).step_by(input.width as usize) {
+            let y = (index as i32) / width;
+            result += height - 2 - y;
+        }
+    }
+
+    result
+}
+
+pub fn part2(input: &Input) -> i32 {
+    let Input { width, height, .. } = input;
+
+    let rounded = &mut input.rounded.clone();
     let mut seen = FastMap::with_capacity(100);
-    seen.insert(grid.clone(), 0);
 
     // Find cycle
     let (start, end) = loop {
-        north(grid);
-        west(grid);
-        south(grid);
-        east(grid);
+        tilt(rounded, &input.fixed_north, &input.roll_north, *width as i16);
+        tilt(rounded, &input.fixed_west, &input.roll_west, 1);
+        tilt(rounded, &input.fixed_south, &input.roll_south, -(*width) as i16);
+        let state = tilt(rounded, &input.fixed_east, &input.roll_east, -1);
 
-        if let Some(previous) = seen.insert(grid.clone(), seen.len()) {
+        if let Some(previous) = seen.insert(state, seen.len()) {
             break (previous, seen.len());
         }
     };
 
-    let offset = 1_000_000_000 - start;
+    let offset = 1_000_000_000 - 1 - start;
     let cycle_width = end - start;
     let remainder = offset % cycle_width;
     let target = start + remainder;
 
-    let (grid, _) = seen.iter().find(|(_, &i)| i == target).unwrap();
-    load(grid)
-}
+    let (state, _) = seen.iter().find(|(_, &i)| i == target).unwrap();
+    let mut result = 0;
 
-fn north(grid: &mut Grid<u8>) {
-    for x in 0..grid.width {
-        let mut fixed = 0;
-
-        for y in 0..grid.height {
-            let point = Point::new(x, y);
-            match grid[point] {
-                b'O' => {
-                    if y > fixed {
-                        grid[point] = b'.';
-                        grid[Point::new(x, fixed)] = b'O';
-                    }
-                    fixed += 1;
-                }
-                b'#' => fixed = y + 1,
-                _ => (),
-            }
-        }
-    }
-}
-
-fn west(grid: &mut Grid<u8>) {
-    for y in 0..grid.height {
-        let mut fixed = 0;
-
-        for x in 0..grid.width {
-            let point = Point::new(x, y);
-            match grid[point] {
-                b'O' => {
-                    if x > fixed {
-                        grid[point] = b'.';
-                        grid[Point::new(fixed, y)] = b'O';
-                    }
-                    fixed += 1;
-                }
-                b'#' => fixed = x + 1,
-                _ => (),
-            }
-        }
-    }
-}
-
-fn south(grid: &mut Grid<u8>) {
-    for x in 0..grid.width {
-        let mut fixed = grid.height - 1;
-
-        for y in (0..grid.height).rev() {
-            let point = Point::new(x, y);
-            match grid[point] {
-                b'O' => {
-                    if y < fixed {
-                        grid[point] = b'.';
-                        grid[Point::new(x, fixed)] = b'O';
-                    }
-                    fixed -= 1;
-                }
-                b'#' => fixed = y - 1,
-                _ => (),
-            }
-        }
-    }
-}
-
-fn east(grid: &mut Grid<u8>) {
-    for y in 0..grid.height {
-        let mut fixed = grid.width - 1;
-
-        for x in (0..grid.width).rev() {
-            let point = Point::new(x, y);
-            match grid[point] {
-                b'O' => {
-                    if x < fixed {
-                        grid[point] = b'.';
-                        grid[Point::new(fixed, y)] = b'O';
-                    }
-                    fixed -= 1;
-                }
-                b'#' => fixed = x - 1,
-                _ => (),
-            }
-        }
-    }
-}
-
-fn load(grid: &Grid<u8>) -> i32 {
-    let mut load = 0;
-
-    for x in 0..grid.width {
-        for y in 0..grid.height {
-            if grid[Point::new(x, y)] == b'O' {
-                load += grid.height - y;
-            }
-        }
+    for (&a, &b) in input.roll_east.iter().zip(state.iter()) {
+        let n = (a - b) as i32;
+        let y = (a as i32) / width;
+        result += n * (height - 1 - y);
     }
 
-    load
+    result
+}
+
+fn tilt(rounded: &mut [i16], fixed: &[i16], roll: &[i16], direction: i16) -> Vec<i16> {
+    let mut state = roll.to_vec();
+
+    for rock in rounded {
+        let index = fixed[*rock as usize] as usize;
+        state[index] += direction;
+        *rock = state[index];
+    }
+
+    state
 }
