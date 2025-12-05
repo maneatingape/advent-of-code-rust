96abe5c3e935f6fddab68f47cc87de93011f1e42
maneatingape <44142177+maneatingape@users.noreply.github.com>
2025-12-05 16:09:50 +0000

    Remove calculation from parse method

--- src/year2025/day02.rs
+++ src/year2025/day02.rs
@@ -9,22 +9,19 @@ const FIRST: [Range; 5] = [[2, 1], [4, 2], [6, 3], [8, 4], [10, 5]];
 const SECOND: [Range; 6] = [[3, 1], [5, 1], [6, 2], [7, 1], [9, 3], [10, 2]];
 const THIRD: [Range; 2] = [[6, 1], [10, 1]];
 
-pub fn parse(input: &str) -> Pair {
-    let ranges: Vec<_> = input.iter_unsigned::<u64>().chunk::<2>().collect();
-    let part_one = sum(&FIRST, &ranges);
-    let part_two = part_one + sum(&SECOND, &ranges) - sum(&THIRD, &ranges);
-    [part_one, part_two]
+pub fn parse(input: &str) -> Vec<Pair> {
+    input.iter_unsigned::<u64>().chunk::<2>().collect()
 }
 
-pub fn part1(input: &Pair) -> u64 {
-    input[0]
+pub fn part1(input: &[Pair]) -> u64 {
+    sum(&FIRST, input)
 }
 
-pub fn part2(input: &Pair) -> u64 {
-    input[1]
+pub fn part2(input: &[Pair]) -> u64 {
+    sum(&FIRST, input) + sum(&SECOND, input) - sum(&THIRD, input)
 }
 
-fn sum(ranges: &[Range], ids: &[Pair]) -> u64 {
+fn sum(ranges: &[Range], input: &[Pair]) -> u64 {
     let mut result = 0;
 
     for &[digits, size] in ranges {
@@ -35,7 +32,7 @@ fn sum(ranges: &[Range], ids: &[Pair]) -> u64 {
         let start = step * (size_power / 10);
         let end = step * (size_power - 1);
 
-        for &[from, to] in ids {
+        for &[from, to] in input {
             let lower = from.next_multiple_of(step).max(start);
             let upper = to.min(end);
 
