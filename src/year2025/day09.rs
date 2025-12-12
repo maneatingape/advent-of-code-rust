b9d4ebbca0f8c9b470449c5e09bc21e2ecdec3a7
rdh <98863820+rhysdh540@users.noreply.github.com>
2025-12-12 04:49:35 -0500

    fast

--- src/year2025/day09.rs
+++ src/year2025/day09.rs
@@ -1,13 +1,5 @@
-use crate::util::grid::*;
-use crate::util::hash::*;
 use crate::util::iter::*;
 use crate::util::parse::*;
-use crate::util::point::*;
-use std::collections::VecDeque;
-
-const OUTSIDE: i64 = 0;
-const INSIDE: i64 = 1;
-const UNKNOWN: i64 = 2;
 
 type Tile = [u64; 2];
 
@@ -18,87 +10,75 @@ pub fn parse(input: &str) -> Vec<Tile> {
 pub fn part1(tiles: &[Tile]) -> u64 {
     let mut area = 0;
 
-    for (i, &[x1, y1]) in tiles.iter().enumerate() {
-        for &[x2, y2] in tiles.iter().skip(i + 1) {
-            let dx = x1.abs_diff(x2) + 1;
-            let dy = y1.abs_diff(y2) + 1;
-            area = area.max(dx * dy);
+    for (i, p1) in tiles.iter().enumerate() {
+        for p2 in tiles.iter().skip(i + 1) {
+            area = area.max(box_area(p1, p2));
         }
     }
 
     area
+}
+
+fn box_area(p1: &Tile, p2: &Tile) -> u64 {
+    let dx = p1[0].abs_diff(p2[0]) + 1;
+    let dy = p1[1].abs_diff(p2[1]) + 1;
+    dx * dy
 }
 
 pub fn part2(tiles: &[Tile]) -> u64 {
     let size = tiles.len();
-    let shrink_x = shrink(tiles, 0);
-    let shrink_y = shrink(tiles, 1);
-    let shrunk: Vec<_> = tiles.iter().map(|&[x, y]| (shrink_x[&x], shrink_y[&y])).collect();
+    let top_index = size / 2;
+    let bottom_index = top_index + 1;
+    let top = &tiles[top_index];
+    let bottom = &tiles[bottom_index];
 
-    let mut area = 0;
-    let mut todo = VecDeque::from([ORIGIN]);
-    let mut grid = Grid::new(shrink_x.len() as i32, shrink_y.len() as i32, UNKNOWN);
+    let mut top_max_y = top[1];
+    for (a, b) in tiles[..=top_index + 1].windows(2).map(|w| (&w[0], &w[1])) {
+        if a[0] == b[0] && a[0] == top[0] {
+            top_max_y = a[1].max(b[1]);
+            break;
+        } else if (a[0].min(b[0])..=a[0].max(b[0])).contains(&top[0]) {
+            top_max_y = a[1];
+            break;
+        }
+    }
 
-    for i in 0..size {
-        let (x1, y1, x2, y2) = minmax(shrunk[i], shrunk[(i + 1) % size]);
+    let mut bottom_min_y = bottom[1];
+    for (a, b) in tiles[bottom_index..].windows(2).map(|w| (&w[0], &w[1])).rev() {
+        if a[0] == b[0] && a[0] == bottom[0] {
+            bottom_min_y = a[1].min(b[1]);
+            break;
+        } else if (a[0].min(b[0])..=a[0].max(b[0])).contains(&bottom[0]) {
+            bottom_min_y = a[1];
+            break;
+        }
+    }
 
-        for x in x1..x2 + 1 {
-            for y in y1..y2 + 1 {
-                grid[Point::new(x, y)] = INSIDE;
+    let mut top_left = top;
+    let mut max_x = 0;
+    for p in tiles[..top_index].iter().rev() {
+        if p[1] <= top_max_y {
+            max_x = max_x.max(p[0]);
+            if (p[1] > top_left[1] || p[0] < top_left[0]) && p[0] >= max_x {
+                top_left = p;
             }
+        } else {
+            break;
         }
     }
 
-    while let Some(point) = todo.pop_front() {
-        for next in ORTHOGONAL.map(|o| point + o) {
-            if grid.contains(next) && grid[next] == UNKNOWN {
-                grid[next] = OUTSIDE;
-                todo.push_back(next);
+    let mut bottom_left = bottom;
+    max_x = 0;
+    for p in tiles[bottom_index + 1..].iter() {
+        if p[1] >= bottom_min_y {
+            max_x = max_x.max(p[0]);
+            if (p[1] < bottom_left[1] || p[0] < bottom_left[0]) && p[0] >= max_x {
+                bottom_left = p;
             }
+        } else {
+            break;
         }
     }
 
-    for y in 1..grid.height {
-        for x in 1..grid.width {
-            let point = Point::new(x, y);
-            let value = i64::from(grid[point] != OUTSIDE);
-            grid[point] = value + grid[point + UP] + grid[point + LEFT] - grid[point + UP + LEFT];
-        }
-    }
-
-    for i in 0..size {
-        for j in i + 1..size {
-            let (x1, y1, x2, y2) = minmax(shrunk[i], shrunk[j]);
-
-            let expected = (x2 - x1 + 1) as i64 * (y2 - y1 + 1) as i64;
-            let actual = grid[Point::new(x2, y2)]
-                - grid[Point::new(x1 - 1, y2)]
-                - grid[Point::new(x2, y1 - 1)]
-                + grid[Point::new(x1 - 1, y1 - 1)];
-
-            if expected == actual {
-                let [x1, y1] = tiles[i];
-                let [x2, y2] = tiles[j];
-                let dx = x1.abs_diff(x2) + 1;
-                let dy = y1.abs_diff(y2) + 1;
-                area = area.max(dx * dy);
-            }
-        }
-    }
-
-    area
-}
-
-fn shrink(tiles: &[Tile], index: usize) -> FastMap<u64, i32> {
-    let mut axis: Vec<_> = tiles.iter().map(|tile| tile[index]).collect();
-    axis.push(u64::MIN);
-    axis.push(u64::MAX);
-    axis.sort_unstable();
-    axis.dedup();
-    axis.iter().enumerate().map(|(i, &n)| (n, i as i32)).collect()
-}
-
-#[inline]
-fn minmax((x1, y1): (i32, i32), (x2, y2): (i32, i32)) -> (i32, i32, i32, i32) {
-    (x1.min(x2), y1.min(y2), x1.max(x2), y1.max(y2))
+    box_area(top_left, top).max(box_area(bottom_left, bottom))
 }
