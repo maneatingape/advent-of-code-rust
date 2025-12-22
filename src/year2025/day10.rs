fef58371355b310649a2f09de7d79cc34b2a589a
maneatingape <44142177+maneatingape@users.noreply.github.com>
2025-12-22 13:43:05 +0000

    Faster approach that restricts dimensions more effectively
    Choose smallest range
    
    Arrays instead of vec

--- src/year2025/day10.rs
+++ src/year2025/day10.rs
@@ -1,35 +1,44 @@
 //! # Factory
 use crate::util::bitset::*;
+use crate::util::math::*;
 use crate::util::parse::*;
-use crate::util::thread::*;
-use std::collections::BTreeSet;
+use std::array::from_fn;
 
-type Machine = (usize, Vec<usize>, Vec<i32>);
-type Input = (Vec<i32>, Vec<i32>);
+const SIZE: usize = 16;
 
-/// Each machine can be processed independently, parallelizing the work over multiple threads.
-pub fn parse(input: &str) -> Input {
-    let items: Vec<_> = input.lines().collect();
-
-    let result: Vec<Vec<_>> = spawn_parallel_iterator(&items, |iter| {
-        iter.map(|line| {
-            let machine = parse_machine(line);
-            (configure_lights(&machine), configure_joltages(&machine))
-        })
-        .collect()
-    });
-
-    result.into_iter().flatten().unzip()
+pub struct Machine {
+    lights: u32,
+    buttons: Vec<u32>,
+    joltages: Vec<i32>,
 }
 
-pub fn part1(input: &Input) -> i32 {
-    let (presses, _) = input;
-    presses.iter().sum()
+struct Subspace {
+    rank: usize,
+    nullity: usize,
+    lcm: i32,
+    particular_solution: i32,
+    rhs: [i32; SIZE],
+    basis: Vec<Basis>,
 }
 
-pub fn part2(input: &Input) -> i32 {
-    let (_, presses) = input;
-    presses.iter().sum()
+#[derive(Clone, Copy)]
+struct Basis {
+    limit: i32,
+    cost: i32,
+    vs: [i32; SIZE],
+}
+
+pub fn parse(input: &str) -> Vec<Machine> {
+    input.lines().map(parse_machine).collect()
+}
+
+pub fn part1(input: &[Machine]) -> u32 {
+    let mut todo = Vec::with_capacity(1_000);
+    input.iter().map(|machine| configure_lights(machine, &mut todo)).sum()
+}
+
+pub fn part2(input: &[Machine]) -> i32 {
+    input.iter().map(configure_joltages).sum()
 }
 
 /// Convert light patterns and buttons to bitmasks to speed up part one.
@@ -37,209 +46,214 @@ fn parse_machine(line: &str) -> Machine {
     let tokens: Vec<_> = line.split_ascii_whitespace().collect();
     let last = tokens.len() - 1;
 
-    let lights = tokens[0]
+    let lights = tokens[0][1..]
         .bytes()
-        .skip(1)
         .enumerate()
-        .fold(0, |light, (i, b)| light | (usize::from(b == b'#') << i));
+        .fold(0, |light, (i, b)| light | (u32::from(b == b'#') << i));
     let buttons = tokens[1..last]
         .iter()
-        .map(|token| token.iter_unsigned::<usize>().fold(0, |button, i| button | (1 << i)))
+        .map(|token| token.iter_unsigned().fold(0, |button, i: u32| button | (1 << i)))
         .collect();
-    let joltages = tokens[last].iter_signed::<i32>().collect();
+    let joltages = tokens[last].iter_signed().collect();
 
-    (lights, buttons, joltages)
+    Machine { lights, buttons, joltages }
 }
 
-/// Check all patterns with one set bit, then pattern with two sets bits and so on, until we find
-/// a match.
-fn configure_lights((lights, buttons, _): &Machine) -> i32 {
-    let limit = 1 << buttons.len();
-    let mut set = 0;
+/// Check all patterns with one set bit, then patterns with two sets bits and so on,
+/// returning early as soon as we find a match without checking all possible combinations.
+fn configure_lights(machine: &Machine, todo: &mut Vec<(usize, u32, u32)>) -> u32 {
+    todo.clear();
+    todo.push((machine.buttons.len(), 0, 0));
 
-    loop {
-        set += 1;
-        let mut n = (1 << set) - 1;
+    for index in 0.. {
+        let (limit, pressed, pattern) = todo[index];
 
-        while n < limit {
-            if *lights == n.biterator().fold(0, |acc, i| acc ^ buttons[i]) {
-                return set;
+        for i in 0..limit {
+            let next_pattern = pattern ^ machine.buttons[i];
+            if next_pattern == machine.lights {
+                return pressed + 1;
             }
-            n = next_same_bits(n);
+            todo.push((i, pressed + 1, next_pattern));
         }
     }
+
+    unreachable!()
 }
 
-/// Find the next highest integer with the same number of one bits as the previous integer,
-/// for example 1011 => 1110.
-fn next_same_bits(n: i32) -> i32 {
-    let smallest = n & -n;
-    let ripple = n + smallest;
-    let ones = n ^ ripple;
-    let next = (ones >> 2) / smallest;
-    ripple | next
+/// Convert the buttons and joltages to simultaenous equations,
+/// then use [Gaussian Elimination](https://en.wikipedia.org/wiki/Gaussian_elimination)
+/// to reduce (the up to 11) dimensions of the full solution space to a (between 0 and 4)
+/// dimensional subspace of only the free variables.
+fn configure_joltages(machine: &Machine) -> i32 {
+    let subspace = gaussian_elimation(machine);
+    let Subspace { nullity, particular_solution, lcm, rhs, .. } = subspace;
+
+    if nullity == 0 {
+        return particular_solution / lcm;
+    }
+
+    let remaining = (1 << subspace.basis.len()) - 1;
+    recurse(&subspace, remaining, rhs, particular_solution).unwrap()
 }
 
-/// Convert the buttons and joltages to equations, then use
-/// [Gaussian Elimination](https://en.wikipedia.org/wiki/Gaussian_elimination) to reduce the
-/// dimensioanlity of the problem to only the free variables.
-fn configure_joltages((_, buttons, joltages): &Machine) -> i32 {
+fn gaussian_elimation(machine: &Machine) -> Subspace {
+    let Machine { buttons, joltages, .. } = machine;
     let width = buttons.len();
     let height = joltages.len();
-    let mut equations = vec![vec![0; width + 1]; height];
-    let mut limit = vec![i32::MAX; width];
-    let mut previous: BTreeSet<_> = (0..width).collect();
-    let mut current: BTreeSet<_> = BTreeSet::new();
+    assert!(width < 15 && height < 15, "Expect at most 15 buttons and 15 joltages");
 
-    // If a button can increment a joltage counter then it get a coefficent of 1, otherwise zero.
-    // Using the first example machine and labelling the button a..f from left to right:
-    //
-    // * [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-    // * d + f = 3
-    // * b + f = 5
-    // * c + d + e = 4
-    // * a + b + d = 7
-    for (row, &joltage) in joltages.iter().enumerate() {
-        equations[row][width] = joltage;
+    let mut equations = [[0; SIZE]; SIZE];
 
-        for col in 0..width {
-            if buttons[col] & (1 << row) != 0 {
-                equations[row][col] = 1;
-                limit[col] = limit[col].min(joltage);
-            }
-        }
+    for row in 0..height {
+        equations[row][width] = joltages[row];
     }
 
-    // Gaussian elimination to reduce the matrix to row echelon form. For example the previous
-    // equations form a matrix:
-    //
-    // [ 0  0  0  1  0  1 | 3 ]
-    // [ 0  1  0  0  0  1 | 4 ]
-    // [ 0  0  1  1  1  0 | 5 ]
-    // [ 1  0  0  1  0  0 | 7 ]
-    while previous != current {
-        previous = current;
-        current = (0..width).collect();
+    for col in 0..width {
+        let mut limit = i32::MAX;
 
-        let mut pivot_row = 0;
-        let mut pivot_col = 0;
+        for row in buttons[col].biterator() {
+            equations[row][col] = 1;
+            limit = limit.min(joltages[row]);
+        }
 
-        while pivot_row < height && pivot_col < width {
-            let Some(found) = (pivot_row..height).find(|&row| {
-                let coefficient = equations[row][pivot_col];
-                coefficient != 0 && equations[row].iter().all(|c| c % coefficient == 0)
-            }) else {
-                pivot_col += 1;
-                continue;
-            };
+        equations[height][col] = limit;
+    }
 
-            equations.swap(pivot_row, found);
-            let coefficient = equations[pivot_row][pivot_col];
-            equations[pivot_row].iter_mut().for_each(|c| *c /= coefficient);
+    let mut rank = 0;
+    let mut last = width;
+
+    while rank < height && rank < last {
+        if let Some(found) = (rank..height)
+            .filter(|&row| equations[row][rank] != 0)
+            .min_by_key(|&row| equations[row][rank].abs())
+        {
+            equations.swap(rank, found);
+            let mut pivot = equations[rank][rank];
+
+            if pivot < 0 {
+                pivot *= -1;
+                equations[rank][rank..=width].iter_mut().for_each(|c| *c *= -1);
+            }
 
             for row in 0..height {
-                if row != pivot_row {
-                    let coefficient = equations[row][pivot_col];
-                    let [from, to] = equations.get_disjoint_mut([pivot_row, row]).unwrap();
-                    from.iter().zip(to).for_each(|(f, t)| *t -= coefficient * f);
+                let coefficient = equations[row][rank];
+                if row != rank && coefficient != 0 {
+                    let lcm = coefficient.abs().lcm(pivot);
+                    let x = lcm / coefficient.abs();
+                    let y = lcm / pivot * coefficient.signum();
+
+                    for col in 0..equations[row].len() {
+                        equations[row][col] = x * equations[row][col] - y * equations[rank][col];
+                    }
                 }
             }
 
-            current.remove(&pivot_col);
-            pivot_row += 1;
-            pivot_col += 1;
+            rank += 1;
+        } else {
+            last -= 1;
+            equations[..=height].iter_mut().for_each(|row| row.swap(rank, last));
         }
     }
 
-    if current.is_empty() {
-        return (0..height).map(|row| equations[row][width]).sum();
+    let lcm = (0..rank).fold(1, |lcm, pivot| lcm.lcm(equations[pivot][pivot]));
+
+    for (pivot, equation) in equations[..rank].iter_mut().enumerate() {
+        let q = lcm / equation[pivot];
+        equation[rank..=width].iter_mut().for_each(|c| *c *= q);
     }
 
-    // Brute force search over the free variables.
-    let free = current.len();
-    let fixed = width - current.len();
-    let presses = (0..fixed).map(|row| equations[row][width]).sum::<i32>();
-    let mut cost = vec![0; free];
-    let mut coefficients = vec![vec![0; height]; free];
-    let mut rhs = vec![vec![0; height]; free];
-    let mut ordered_limit: Vec<_> = vec![0; free];
+    let nullity = width - rank;
+    let rhs = from_fn(|row| equations[row][width]);
+    let particular_solution = rhs[..rank].iter().sum();
+    let basis: Vec<_> = (0..nullity)
+        .map(|col| {
+            let limit = equations[height][col + rank];
+            let vs = from_fn(|row| equations[row][rank + col]);
+            let cost = lcm - vs[..rank].iter().sum::<i32>();
+            Basis { limit, cost, vs }
+        })
+        .collect();
 
-    for (to, &from) in current.iter().enumerate() {
-        cost[to] = 1 - (0..fixed).map(|row| equations[row][from]).sum::<i32>();
-        ordered_limit[to] = limit[from];
-
-        for row in 0..height {
-            coefficients[to][row] = equations[row][from];
-        }
-    }
-
-    for row in 0..height {
-        rhs[0][row] = equations[row][width];
-    }
-
-    recurse(&cost, &ordered_limit, &coefficients, &mut rhs, fixed, presses, 0).unwrap()
+    Subspace { rank, nullity, lcm, particular_solution, rhs, basis }
 }
 
 fn recurse(
-    cost: &[i32],
-    limit: &[i32],
-    coefficients: &[Vec<i32>],
-    rhs: &mut [Vec<i32>],
-    fixed: usize,
+    subspace: &Subspace,
+    remaining: usize,
+    mut rhs: [i32; SIZE],
     presses: i32,
-    depth: usize,
 ) -> Option<i32> {
-    let height = rhs[depth].len();
+    let rank = subspace.rank;
+    let mut temp = rhs;
 
-    if depth == coefficients.len() - 1 {
-        // For the last free variables, we can use the remaining inequalities (and possibily and
-        // equalities to find the answer immediately) without needed another search. This
-        // reduces the dimensions of the search space by one.
+    for i in remaining.biterator() {
+        let free = &subspace.basis[i];
+        for (row, &v) in free.vs[..rank].iter().enumerate() {
+            if v < 0 {
+                temp[row] -= v * free.limit;
+            }
+        }
+    }
+
+    let mut min_value = i32::MAX;
+    let mut min_index = usize::MAX;
+    let mut global_lower = 0;
+    let mut global_upper = 0;
+
+    for i in remaining.biterator() {
+        let free = &subspace.basis[i];
         let mut lower = 0;
-        let mut upper = limit[depth];
+        let mut upper = free.limit;
 
-        // Check inequalites
-        for (&coef, &rhs) in coefficients[depth].iter().zip(&rhs[depth]) {
-            if rhs >= 0 {
-                if coef > 0 {
-                    upper = upper.min(rhs / coef);
-                }
-            } else if coef < 0 {
-                let floor = (rhs + coef + 1) / coef;
-                lower = lower.max(floor);
-            } else {
-                upper = -1;
+        for (&v, &rhs) in free.vs[..rank].iter().zip(&temp) {
+            if v > 0 {
+                upper = upper.min(rhs / v);
+            }
+            if v < 0 {
+                let rhs = rhs + v * free.limit;
+                lower = lower.max((rhs + v + 1) / v);
             }
         }
 
-        // Check equalities (if they exist)
-        for row in fixed..height {
-            let c = coefficients[depth][row];
-            let r = rhs[depth][row];
-
-            if c != 0 {
-                if r % c == 0 {
-                    upper = upper.min(r / c);
-                    lower = lower.max(r / c);
-                } else {
-                    upper = -1;
-                }
-            }
+        let size = upper - lower + 1;
+        if size > 0 && size < min_value {
+            min_value = size;
+            min_index = i;
+            global_lower = lower;
+            global_upper = upper;
         }
+    }
 
-        let presses = presses + cost[depth] * if cost[depth] >= 0 { lower } else { upper };
-        (lower <= upper).then_some(presses)
-    } else {
-        (0..=limit[depth])
-            .filter_map(|x| {
-                let next_presses = presses + x * cost[depth];
+    if min_index == usize::MAX {
+        return None;
+    }
 
-                for row in 0..height {
-                    rhs[depth + 1][row] = rhs[depth][row] - x * coefficients[depth][row];
-                }
+    let remaining = remaining ^ (1 << min_index);
+    let lower = global_lower;
+    let upper = global_upper;
+    let Basis { cost, vs, .. } = &subspace.basis[min_index];
+    let cost = *cost;
+    let lcm = subspace.lcm;
 
-                recurse(cost, limit, coefficients, rhs, fixed, next_presses, depth + 1)
+    if remaining != 0 {
+        rhs[..rank].iter_mut().zip(vs).for_each(|(rhs, v)| *rhs -= lower * v);
+
+        (lower..upper + 1)
+            .filter_map(|n| {
+                let result = recurse(subspace, remaining, rhs, presses + n * cost);
+                rhs[..rank].iter_mut().zip(vs).for_each(|(rhs, v)| *rhs -= v);
+                result
             })
             .min()
+    } else if cost >= 0 {
+        (lower..upper + 1).find_map(|n| {
+            let total = (presses + n * cost) / lcm;
+            rhs[..rank].iter().zip(vs).all(|(rhs, v)| (rhs - n * v) % lcm == 0).then_some(total)
+        })
+    } else {
+        (lower..upper + 1).rev().find_map(|n| {
+            let total = (presses + n * cost) / lcm;
+            rhs[..rank].iter().zip(vs).all(|(rhs, v)| (rhs - n * v) % lcm == 0).then_some(total)
+        })
     }
 }
