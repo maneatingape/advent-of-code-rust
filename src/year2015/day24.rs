419c91f56140cb05e1a113410700a9db39684166
Eric Blake <eblake@redhat.com>
2026-01-27 06:49:51 -0600

    Use dynamic programming for 2015 day 24
    This day is somewhat related to day 17 (partitioning a set of small
    integers).  But where day 17 only cared about how many groups were
    possible, this day also needs to know the contents of those groups in
    order to compute quantum entanglement.  Tackle this via a two-part
    solution: first, do a dynamic programming pass in O(r*n) (where r is
    sum/3) that computes which group sizes and items are worth
    investigating at any given sum.  Then, do a depth first search with
    memoization (which can be shared between parts 1 and 2) that filters
    out branches where the requested group size and maximum item index are
    not feasible.
    
    The two parts overlap quite a bit of the cache hits.  I added
    temporary instrumentation on how many cache hits and writes occur; on
    my input, when best3 is computed first:
    solve(3) had 140 hits, 8988 writes
    solve(4) had 119 hits, 2912 writes
    
    and conversely, when swapping the order:
    solve(4) had 168 hits, 4575 writes
    solve(3) had 91 hits, 7325 writes
    
    so there is enough overlap between the two parts to warrant sharing
    the cache by hoisting all the work into parse.  (With a bit more
    instrumentation, I suppose I could have measured how many recursive
    calls were saved by the later hits.)
    
    On my machine, this changed benchmark results:
    pre-patch: parse 93ns, part1 736us, part2 127us
    post-patch: parse 149us, part1 2ns, part2 2ns

--- src/year2015/day24.rs
+++ src/year2015/day24.rs
@@ -3,57 +3,133 @@
 //! To simplify things assumes that the remaining items after the first best combination is found
 //! can be split evenly.
 //!
-//! Sorts the weights in ascending order, then tries combinations of increasing size until a
-//! match in found. This will be the answer since the package count is the smallest and the
-//! quantum entanglement will also be the lowest.
-use crate::util::bitset::*;
+//! Runs a dynamic programming preliminary pass over the data to
+//! compute a bitmask of which group sizes are reachable and a bitmask
+//! of which item indices were the last encountered while reaching at
+//! least one of those group sizes for a given sum.  This information
+//! can then be used to prune away much of the depth-first search,
+//! with memoization avoiding repeated computations.  Caching is done
+//! via a 3D array; a typical input will have a part 1 sum around 500
+//! with the smallest group having 6 or 7 of the 28 items, so the
+//! cache is smaller than 1 megabyte, and part 2 ends up reusing many
+//! entries already populated during part 1.  Thus, this solution
+//! computes the results during `parse()`, at which point `part1()`
+//! and `part2()` merely return the already-known results.
 use crate::util::parse::*;
 
-pub fn parse(input: &str) -> Vec<u32> {
-    let mut packages: Vec<_> = input.iter_unsigned().collect();
-    packages.sort_unstable();
-    packages
+const MAX_SUM: usize = 600;
+const ITEMS: usize = 28;
+const MAX_GROUP: usize = 7;
+
+#[derive(Clone, Copy)]
+struct Info {
+    sizes: u8, // bitmask, bit 0 set for the empty group, not bothering with more than 7 items in a group
+    items: u32, // bitmask, highest item indices that allowed group to reach sum
 }
 
-pub fn part1(input: &[u32]) -> u64 {
-    combinations(input, 3)
+struct State {
+    info: [Info; MAX_SUM], // Info learned during dynamic programming pre-pass
+    memo: Box<[[[u64; MAX_GROUP + 1]; ITEMS]; MAX_SUM]>, // Memoization cache, u64::MAX for unknown
+    packages: Vec<usize>,  // Package weights from input
+    total: usize,          // Total weight of all packages
 }
 
-pub fn part2(input: &[u32]) -> u64 {
-    combinations(input, 4)
+pub struct Input {
+    best3: u64, // Best quantum entanglement for one-third of total
+    best4: u64, // Best quantum entanglement for one-fourth of total
 }
 
-/// Breadth first search over all possible package combinations as we want the fewest possible
-/// packages that sum to the target weight. Uses a bitmask as a set for each package combination.
-fn combinations(input: &[u32], groups: u32) -> u64 {
-    let target = input.iter().sum::<u32>() / groups;
-    let mut current = &mut Vec::with_capacity(100_000);
-    let mut next = &mut Vec::with_capacity(100_000);
+pub fn parse(input: &str) -> Input {
+    let packages: Vec<_> = input.iter_unsigned().collect();
+    let mut state = State {
+        info: [Info { sizes: 0, items: 0 }; MAX_SUM],
+        memo: Box::new([[[u64::MAX; MAX_GROUP + 1]; ITEMS]; MAX_SUM]),
+        packages: packages.clone(),
+        total: packages.iter().sum::<usize>(),
+    };
+    state.info[0].sizes = 1 << 0;
+    let mut cap = 0;
 
-    // Start with no packages.
-    current.push((0_u32, 0_u32));
-
-    loop {
-        for (weight, packages) in current.drain(..) {
-            // Find the next highest power of two.
-            let start = 32 - packages.leading_zeros() as usize;
-
-            // Add one package at a time to this combination.
-            for i in start..input.len() {
-                let next_weight = weight + input[i];
-                let next_packages = packages | (1 << i);
-
-                if next_weight == target {
-                    return next_packages.biterator().map(|i| input[i] as u64).product();
-                }
-                if next_weight > target {
-                    break;
-                }
-
-                next.push((next_weight, next_packages));
+    // For each item, update which groups are reachable with that item, starting from largest sum.
+    for (rank, size) in state.packages.iter().enumerate() {
+        cap += size;
+        let goal = if cap < state.total / 3 { cap } else { state.total / 3 };
+        for i in (*size..goal + 1).rev() {
+            if state.info[i - *size].sizes != 0 {
+                state.info[i].sizes |= state.info[i - *size].sizes << 1;
+                state.info[i].items |= 1 << rank;
             }
         }
-
-        (current, next) = (next, current);
     }
+
+    Input { best3: solve(&mut state, 3), best4: solve(&mut state, 4) }
+}
+
+pub fn part1(input: &Input) -> u64 {
+    input.best3
+}
+
+pub fn part2(input: &Input) -> u64 {
+    input.best4
+}
+
+/// Convert the msb below limit back into a bit number, or ITEMS if no such bit remains in value.
+fn msb_below(value: u32, limit: usize) -> usize {
+    let remaining = value & ((1 << limit) - 1);
+    if remaining == 0 {
+        return ITEMS;
+    }
+    31 - remaining.leading_zeros() as usize
+}
+
+/// Kick off a depth first search.
+fn solve(state: &mut State, groups: usize) -> u64 {
+    let goal = state.total / groups;
+    let sizebit = state.info[goal].sizes & (-(state.info[goal].sizes as i8) as u8);
+    combinations(
+        state,
+        msb_below(sizebit.into(), ITEMS),
+        msb_below(state.info[goal].items, ITEMS),
+        goal,
+    )
+}
+
+/// Depth first search over all possible package combinations, using
+/// `state::info` to focus on only the best quantum entanglement that
+/// satisfy the given group size and maximum item index that reach the
+/// given sum, and `state::memo` as a cache shared between parts.
+/// Returns `u64::MAX` if this branch is not viable.
+fn combinations(state: &mut State, size: usize, maxindex: usize, sum: usize) -> u64 {
+    // Check for bottom of the recursion
+    if size == 0 && sum == 0 {
+        return 1;
+    }
+
+    // Impossible to proceed if size is not supported at this sum
+    if ((1 << size) & state.info[sum].sizes) == 0 {
+        return u64::MAX;
+    }
+
+    // Impossible to proceed if no smaller item indices remain
+    if maxindex == ITEMS {
+        return u64::MAX;
+    }
+
+    // Check if answer still needs to be computed
+    if state.memo[sum][maxindex][size] == u64::MAX {
+        let nextsum = sum - state.packages[maxindex];
+        let by_group =
+            combinations(state, size - 1, msb_below(state.info[nextsum].items, maxindex), nextsum);
+        let by_index = combinations(state, size, msb_below(state.info[sum].items, maxindex), sum);
+        let entry = &mut state.memo[sum][maxindex][size];
+        if by_group < u64::MAX && by_group * (state.packages[maxindex] as u64) < *entry {
+            *entry = by_group * (state.packages[maxindex] as u64);
+        }
+        if by_index < *entry {
+            *entry = by_index;
+        }
+    }
+
+    // Return cached answer
+    state.memo[sum][maxindex][size]
 }
