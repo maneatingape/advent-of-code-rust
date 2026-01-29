3e3b5a95fffc7a181fa6844dd1e6c0a3ef255fdd
Eric Blake <eblake@redhat.com>
2026-01-29 09:53:56 -0600

    Only permute over half the space, when it makes sense
    Several days, starting with 2015 day 9, perform a permutation over
    graphs with symmetric weights (or weights that can easily be made
    symmetric by adding both directions or taking care of which direction
    in a loop was cast out).  As such, it is possible to get the same
    result by skipping permutations that are lexically reversed from a
    configuration seen earlier (ie. the path a->b->c gives the same
    results as c->b->a).
    
    Heap's algorithm does not make half-permutations easy.  However,
    several other algorithms do; among them are the Single-Track and
    Steinhaus-Johnson-Trotter algorithms.  I took a shot at implementing
    SJT by transpiling from Trotter's original Algol implementation[1],
    including its worst-case O(n) work in determining elements to swap in
    one iteration, although most iterations do not encounter the worst
    case.  Shimon Even documented a way to speed it up to amortized O(1)
    work per callback (the same as what Heap's algorithm guarantees), but
    I have not yet found a good source that I can copy from for
    implementing that speed-up.  Still, even with room for potential
    improvements in the iterator, the real savings are in the fact that
    I've cut the number of iterations in half.
    
    [1] https://dl.acm.org/doi/epdf/10.1145/368637.368660
    
    On my machine, cargo bench says I got the following speedups:
    2015 day 9: 68us -> 27us
    2015 day 13: 69us -> 33us
    2016 day 24: 604us -> 541us
    
    Year 2019 day 7 also uses permutations(), but it does not involve
    traveling a loop, so it cannot be cut in half.

--- src/year2015/day09.rs
+++ src/year2015/day09.rs
@@ -4,8 +4,9 @@
 //! [Travelling Salesman Problem](https://en.wikipedia.org/wiki/Travelling_salesman_problem).
 //!
 //! There are 8 locations, so naively it would require checking 8! = 40,320 permutations. We can
-//! reduce this to 7! = 5,040 permutations by arbitrarily choosing one of the locations as the
-//! start.
+//! reduce this to 7!/2 = 2,520 permutations by arbitrarily choosing one of the locations as the
+//! start, and skipping lexically reversed permutations (since the path a->b->c has the same
+//! length as c->b->a).
 //!
 //! We then compute the distance to complete the trip and return to the original location.
 //! Since the problem does not ask us to end up in the same location we then "break" the cycle.
@@ -13,11 +14,11 @@
 //! longest journey we remove the shortest single journey.
 //!
 //! For speed we first convert each location into an index, then store the distances between
-//! every pair of locations in an array for fast lookup. Our utility [`permutations`] method uses
-//! [Heap's algorithm](https://en.wikipedia.org/wiki/Heap%27s_algorithm) for efficiency,
+//! every pair of locations in an array for fast lookup. Our utility [`half_permutations`] method uses
+//! [Steinhaus-Johnson-Trotter's algorithm](https://en.wikipedia.org/wiki/Steinhaus%E2%80%93Johnson%E2%80%93Trotter_algorithm) for efficiency,
 //! modifying the slice in place.
 //!
-//! [`permutations`]: crate::util::slice
+//! [`half_permutations`]: crate::util::slice
 use crate::util::hash::*;
 use crate::util::iter::*;
 use crate::util::parse::*;
@@ -53,7 +54,7 @@ pub fn parse(input: &str) -> Result {
     let mut global_max = u32::MIN;
     let mut indices: Vec<_> = (1..stride).collect();
 
-    indices.permutations(|slice| {
+    indices.half_permutations(|slice| {
         let mut sum = 0;
         let mut local_min = u32::MAX;
         let mut local_max = u32::MIN;
