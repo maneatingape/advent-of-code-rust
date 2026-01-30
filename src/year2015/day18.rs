42b8cc21b5fbc2ef88f2fab2767d6b38637596a2
Eric Blake <eblake@redhat.com>
2026-01-29 22:23:18 -0600

    Use denser representation in Game of Life
    In the same vein as the previous patch, observe that visiting 2 ints
    per row * 100 rows per iteration (200 parallel bit operations per
    round) is more expensive than visiting 17 ints per row-group * 10
    row-groups per iteration (170 parallel bit operations), by packing
    10x6 bits into an int rather than 1x50.  It's not quite a 15% savings
    because there are a few more mask operations that must be injected,
    and the parsing is not quite as friendly on cache lines, but it is
    enough to be statistically noticeable.  On my machine, repeated runs
    of 'cargo bench' show an improvement from 56us per part to 53us.

--- src/year2015/day18.rs
+++ src/year2015/day18.rs
@@ -1,10 +1,17 @@
 //! # Like a GIF For Your Yard
 //!
 //! To solve this efficiently we use a [SWAR](https://en.wikipedia.org/wiki/SWAR) approach,
-//! packing 50 lights into a `u64` taking 1 bit each (if the grid were bigger, we ccould use all
-//! 64 bits; but with only two integers per row, it is easier to reuse common masks by keeping
-//! the integers balanced). We calculate the next generation using no conditional statements with
-//! the following steps.
+//! packing 60 lights into a `u64` taking 1 bit each.  Why 60 bits?  A naive packing of 1 row
+//! by 100 bits occupies 2 ints, but requires 2*100 ints to represent the entire grid, and leaves
+//! 28 bits per row idle.  But if we rearrange the bits in a 10x6 mini-grid, sharing a portion
+//! of 10 different rows in a single int, we now require 17 ints to represent one row, but cut
+//! the number of row operations by 10; only 17*10 ints to represent the entire grid, or 15%
+//! denser.  Fewer idle bits makes the overall computation faster.  Note that it is easier
+//! to create an oversized grid where the borders are all zeros, to reduce the special
+//! cases in the inner loop.
+//!
+//! With the grid layout decided, we calculate the next generation using no conditional statements
+//! with the following steps.
 //!
 //! 1. Pack the input bytes into register values that can be represented as binary digits.  Creating
 //!    an extra empty row above and below the grid allows for less special-casing later on.
@@ -18,14 +25,14 @@
 //! 2. Perform a half-adder and full-adder computation of each bit with its vertical neighbors into
 //!    3 temporaries, using only bitwise logic, and shifting in zeroes at the edge.  For each row,
 //!    the two bits carry3#sum3 represent the 2-digit sum of three bits including the central row,
-//!    and a computed carry2 joined with an implied bit sum2 = sum3^orig represent the sum of
+//!    and a computed carry2 joined with an implied bit sum2 = sum3^cell represent the sum of
 //!    only the two neighbors.
 //!
 //! ```none
-//!     10001              carry3 = 11001 = carry2 | (orig&(above^below))
-//!     01011  =>            sum3 = 00111 = above^below^orig
+//!     10001              carry3 = 11001 = carry2 | (cell&(above^below))
+//!     01011  =>            sum3 = 00111 = above^below^cell
 //!     11101              carry2 = 10001 = above&below
-//!                (implied) sum2 = 01100 = sum3^orig
+//!                (implied) sum2 = 01100 = sum3^cell
 //! ```
 //!
 //! 3. Shift values to obtain the horizontal neighbors 1 bit away (or across the integer boundary),
@@ -57,29 +64,47 @@
 //! If we `OR` the neighbor count with the current cell, either `0000` or `0001` then the
 //! binary representation of a lit cell will always be `0011`.
 //!
-//! Using the bits as labelled above, the next cell is `(orig|s) & (q^r) & !p`.
-type Lights = [[u64; 2]; 102];
+//! Using the bits as labelled above, the next cell is `(cell|s) & (q^r) & !p`.
 
-/// Since rows are 100 lights wide, it's easier to just uniformly split between two u64.
-const CELLS_PER_INT: usize = 100 / 2;
+/// Each part runs the Game of Life for 100 iterations.
+const ITERATIONS: usize = 100;
+
+/// Pack a portion of 10 different rows into a single int, with 6 cells over 17 columns per row.
+/// Using the notation `0xL~R` as shorthand for `0bLLLL_LLRR_RRRR`, unused bits as U, and row 10
+/// shown as A, the resulting 64-bit number is `0xU1~2_3~45_~67~_89~A`.
+const CELLS_PER_CHUNK: usize = 6;
+
+/// All other constants used below are derived from the choice above. You can go back to
+/// 100 rows of 2 ints, with 1x50 cells per int, by changing `CELLS_PER_CHUNK` to 50.
+const CHUNKS_PER_INT: usize = 64 / CELLS_PER_CHUNK;
+const CELLS_PER_INT: usize = CHUNKS_PER_INT * CELLS_PER_CHUNK;
+const ALL_BITS_MASK: u64 = (1_u64 << CELLS_PER_INT) - 1;
+const NEIGHBOR_SHIFT: usize = (CHUNKS_PER_INT - 1) * CELLS_PER_CHUNK; // 54
+const BOTTOM_ROW_MASK: u64 = (1 << CELLS_PER_CHUNK) - 1; // All bits from row 10
+const TOP_ROW_MASK: u64 = BOTTOM_ROW_MASK << NEIGHBOR_SHIFT; // All bits from row 1
+const RIGHT_COL_MASK: u64 = ALL_BITS_MASK / BOTTOM_ROW_MASK; // All bits from column 6
+const LEFT_COL_MASK: u64 = RIGHT_COL_MASK << (CELLS_PER_CHUNK - 1); // All bits from column 1
+const INTS_PER_ROW: usize = 100_usize.div_ceil(CELLS_PER_CHUNK); // 17
+const ROW_END_SLOP: usize = INTS_PER_ROW * CELLS_PER_CHUNK - 100; // 2 bits unused in final column
+const ROW_END_MASK: u64 = RIGHT_COL_MASK * (BOTTOM_ROW_MASK - ((1 << ROW_END_SLOP) - 1)); // All bits in last column
+const GROUPS_PER_ITERATION: usize = 100_usize.div_ceil(CHUNKS_PER_INT); // 10
+
+type Lights = [[u64; INTS_PER_ROW + 2]; GROUPS_PER_ITERATION + 2];
 
 /// Pack the lights into 1 bit each in [big-endian order](https://en.wikipedia.org/wiki/Endianness).
 pub fn parse(input: &str) -> Lights {
     let mut grid = default();
 
-    // Reserve blank row above and below for less special-casing.
-    grid[0][0] = 0;
-    grid[0][1] = 0;
+    // Reserve blank int on all four borders: row 0 and 11, column 0 and 18
     for (y, row) in input.lines().enumerate() {
         for (x, col) in row.bytes().enumerate() {
-            let index = x / CELLS_PER_INT;
-            let offset = (CELLS_PER_INT - 1) - (x % CELLS_PER_INT);
+            let index = x / CELLS_PER_CHUNK + 1;
+            let offset = ((CELLS_PER_CHUNK - 1) - (x % CELLS_PER_CHUNK))
+                + CELLS_PER_CHUNK * ((CHUNKS_PER_INT - 1) - (y % CHUNKS_PER_INT));
             let bit = (col & 1) as u64;
-            grid[y + 1][index] |= bit << offset;
+            grid[y / CHUNKS_PER_INT + 1][index] |= bit << offset;
         }
     }
-    grid[101][0] = 0;
-    grid[101][1] = 0;
 
     grid
 }
@@ -98,40 +123,38 @@ fn game_of_life(input: &Lights, part_two: bool) -> u32 {
     let mut sum3 = default();
     let mut carry2 = default();
 
-    for _ in 0..100 {
-        for y in 1..101 {
-            for x in 0..2 {
+    for _ in 0..ITERATIONS {
+        for y in 1..GROUPS_PER_ITERATION + 1 {
+            for x in 1..INTS_PER_ROW + 1 {
                 // Compute temporaries from upper and lower neighbors.
                 let cell = grid[y][x];
-                let above = grid[y - 1][x];
-                let below = grid[y + 1][x];
+                let above = ((cell >> CELLS_PER_CHUNK) | (grid[y - 1][x] << NEIGHBOR_SHIFT))
+                    & ALL_BITS_MASK;
+                let below = ((cell << CELLS_PER_CHUNK) | (grid[y + 1][x] >> NEIGHBOR_SHIFT))
+                    & ALL_BITS_MASK;
                 carry2[y][x] = above & below;
                 sum3[y][x] = above ^ cell ^ below;
                 carry3[y][x] = carry2[y][x] | (cell & (above ^ below));
             }
         }
 
-        for y in 1..101 {
-            for x in 0..2 {
-                // Prepare to merge 3 groups of sums of three into a sum of 9.  Shift one bit right
-                // to move the left neighbor into the current bit lane; vice versa for the right
-                // neighbor; this gets 49 of the 50 bits in place.
+        for y in 1..GROUPS_PER_ITERATION + 1 {
+            for x in 1..INTS_PER_ROW + 1 {
+                // Prepare to merge 3 groups of sums of three into a sum of 9.  Mask off the right
+                // column and shift cell one bit right to pick up 5 left neighbors per chunk, then
+                // merge in the right column shifted 5 bits left of the left neighbor cell to
+                // populate the final bit lane.  Vice versa for the right neighbor cells.
                 let cell = grid[y][x];
-                let mut leftcarry = carry3[y][x] >> 1;
-                let mut leftsum = sum3[y][x] >> 1;
+                let leftcarry = ((carry3[y][x] & !RIGHT_COL_MASK) >> 1)
+                    | ((carry3[y][x - 1] & RIGHT_COL_MASK) << (CELLS_PER_CHUNK - 1));
+                let leftsum = ((sum3[y][x] & !RIGHT_COL_MASK) >> 1)
+                    | ((sum3[y][x - 1] & RIGHT_COL_MASK) << (CELLS_PER_CHUNK - 1));
                 let midcarry = carry2[y][x];
                 let midsum = sum3[y][x] ^ cell;
-                let mut rightcarry = carry3[y][x] << 1;
-                let mut rightsum = sum3[y][x] << 1;
-
-                // Pull in final bit lane from the neighboring cell.
-                if x == 0 {
-                    rightcarry |= carry3[y][1] >> 49;
-                    rightsum |= sum3[y][1] >> 49;
-                } else {
-                    leftcarry |= carry3[y][0] << 49;
-                    leftsum |= sum3[y][0] << 49;
-                }
+                let rightcarry = ((carry3[y][x] & !LEFT_COL_MASK) << 1)
+                    | ((carry3[y][x + 1] & LEFT_COL_MASK) >> (CELLS_PER_CHUNK - 1));
+                let rightsum = ((sum3[y][x] & !LEFT_COL_MASK) << 1)
+                    | ((sum3[y][x + 1] & LEFT_COL_MASK) >> (CELLS_PER_CHUNK - 1));
 
                 // Compute p, q, r, s.
                 let p = (leftcarry & rightcarry) | (midcarry & (leftcarry ^ rightcarry));
@@ -140,17 +163,20 @@ fn game_of_life(input: &Lights, part_two: bool) -> u32 {
                 let s = leftsum ^ midsum ^ rightsum;
 
                 // Calculate the next generation with no conditional statements.
-                // Mask things back to 50 bits.
-                grid[y][x] = (cell | s) & (q ^ r) & !p & ((1 << CELLS_PER_INT) - 1);
+                // Mask things back to 60 bits.
+                grid[y][x] = (cell | s) & (q ^ r) & !p & ALL_BITS_MASK;
             }
+
+            grid[y][INTS_PER_ROW] &= ROW_END_MASK;
         }
 
         // Set corner lights to always on.
         if part_two {
-            grid[1][0] |= 1 << (CELLS_PER_INT - 1);
-            grid[1][1] |= 1;
-            grid[100][0] |= 1 << (CELLS_PER_INT - 1);
-            grid[100][1] |= 1;
+            grid[1][1] |= LEFT_COL_MASK & TOP_ROW_MASK;
+            grid[1][INTS_PER_ROW] |= (RIGHT_COL_MASK & TOP_ROW_MASK) << ROW_END_SLOP;
+            grid[GROUPS_PER_ITERATION][1] |= LEFT_COL_MASK & BOTTOM_ROW_MASK;
+            grid[GROUPS_PER_ITERATION][INTS_PER_ROW] |=
+                (RIGHT_COL_MASK & BOTTOM_ROW_MASK) << ROW_END_SLOP;
         }
     }
 
@@ -158,5 +184,5 @@ fn game_of_life(input: &Lights, part_two: bool) -> u32 {
 }
 
 fn default() -> Lights {
-    [[0; 2]; 102]
+    [[0; INTS_PER_ROW + 2]; GROUPS_PER_ITERATION + 2]
 }
