fac85763b7975ede76b9a00de6b35c0b88382780
Eric Blake <32933908+ebblake@users.noreply.github.com>
2026-02-18 08:41:13 -0600

    Track enough state to get correct answer, faster (#39)
    For 2016 day 11, tracking only how many items are on a floor is
    insufficient to tell the difference between [AM;AG;BG;BM] (when on the
    floor with BG, you can move either direction) and [AM;BG;AG;BM] (when
    on the floor with AG, you can only move to BG); this results in
    undercounting some inputs.  Rewrite the state to instead track a
    sorted list of one-hot encoded pairs tracking the floor that both
    generator and microchip for an unnamed pair are on; sorting the list
    ensures that equivalent setups map to the same state.
    
    Additionally, once we have a correct value for part 1, then part 2 is
    trivial: add 12 more steps per pair of items introduced on floor 1.
    
    Together, this results in a massive speedup!  Pre-patch, my machine
    completed in 1.4ms; post-patch, it finishes in a mere 18us, roughly
    75x faster.
    
    On my input, pre-patch code iterated through 20,419 entries in todo
    before finding the solution; whereas this solution iterates through
    only 274 (plus 746 other results from move_one and move_two that were
    recognized as duplicate states). The work to find neighbors is more
    expensive (and there may still be opportunities for further speedups),
    but the massive reduction in duplicate states plus the time skipped
    for part 2 more than makes up for the complexity.
    
    For the unit test, observe that EXAMPLE only applies to part 1, but
    add a watered-down example that exposed the bug in the previous
    version of the code, and which does have a valid part2 solution.

--- tests/year2016/day11.rs
+++ tests/year2016/day11.rs
@@ -1,19 +1,35 @@
 use aoc::year2016::day11::*;
 
+// From the puzzle, valid for part 1.
 const EXAMPLE: &str = "\
 The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.
 The second floor contains a hydrogen generator.
 The third floor contains a lithium generator.
 The fourth floor contains nothing relevant.";
 
+// Reduced demo where a solution that merely tracks the number of items per floor would result
+// in a too-low result, because it picks a step that would lead to fried microchips.
+const INTERLOCKED: &str = "\
+The first floor contains a lithium generator and a lithium-compatible microchip.
+The second floor contains a hydrogen generator and an elerium generator and a dilithium generator.
+The third floor contains a hydrogen-compatible microchip and an elerium-compatible microchip and a dilithium-compatible microchip.
+The fourth floor contains nothing relevant.";
+
 #[test]
 fn part1_test() {
     let input = parse(EXAMPLE);
     assert_eq!(part1(&input), 11);
+    let input = parse(INTERLOCKED);
+    assert_eq!(part1(&input), 25);
 }
 
 #[test]
 fn part2_test() {
-    let input = parse(EXAMPLE);
-    assert_eq!(part2(&input), 35);
+    // Technically, EXAMPLE has no valid part2 solution. Even if you disregard the unprotected
+    // chips on floor 1 when the two new pairs are added at step 0, the only viable move to step
+    // 1 would be moving both generators; but since there are no other chips on higher floors,
+    // there is nothing available that can be used to get the elevator back to the first floor
+    // without frying the unprotected microchips.
+    let input = parse(INTERLOCKED);
+    assert_eq!(part2(&input), 49);
 }
