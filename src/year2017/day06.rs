dc82ddbb073c9d7f6e2fba11503bcd29ab00d24e
Eric Blake <32933908+ebblake@users.noreply.github.com>
2026-02-28 15:06:25 -0600

    Fix logic bug in 2017 day 06 (#48)
    The comments made a claim that gets violated by some of the input
    files, for example this one scraped from the megathread:
    https://www.reddit.com/r/adventofcode/comments/7hvtoq/comment/dquaosl/
    
    Fix it by manually handling the overflow cases with a slower-but-safer
    array walk, converting back into condensed form once the overflow is
    resolved.  Since overflow is uncommon, it does not penalize overall
    runtime that much.
    
    While at it, use u64 instead of usize to match intent (although I'm
    not sure how much of this repository would work on a 32-bit machine).

--- src/year2017/day06.rs
+++ src/year2017/day06.rs
@@ -1,25 +1,29 @@
 //! # Memory Reallocation
 //!
-//! Looking at the input and the reallocation rules, we make an assertion:
+//! Looking at the input and the reallocation rules, when there is at most one bank with
+//! 15 blocks, it is easy to see that no other bank will exceed 15 after reallocation.
+//! However, some input files have early scenarios where there are a couple of banks with
+//! 15, which can result in the next round or two needing to process 16 or even 17 blocks.
+//! The overflow issue only occurs early; in the long run, the banks settle into a pattern
+//! where overflow does not interfere.
 //!
-//! * No memory bank will ever exceed 15 blocks.
+//! With that in mind, it is possible to design a compact layout that stores each bank in
+//! one nibble of a u64 in the common case, but with manual iterations managing the overflow
+//! as needed.
 //!
-//! This has a nice effect that we can store the entire memory layout packed into a single
-//! `u64` with each memory bank represented by a nibble.
-//!
-//! This makes it very fast to find the highest nibble using bitwise logic. To detect the cycle
-//! a [`FastMap`] stores each previously seen memory layout along with the cycle that it first
-//! appeared.
+//! For all but the few manual overflow cases, it is very fast to find the highest nibble
+//! using bitwise logic. To detect the cycle a [`FastMap`] stores each previously seen
+//! memory layout along with the cycle that it first appeared.
 use crate::util::hash::*;
 use crate::util::parse::*;
 
 type Input = (u32, u32);
 
 /// Reallocate a bank and set to zero by rotating this mask the correct number of bits.
-const REMOVE: usize = 0x0fffffffffffffff;
-/// The highest number of banks possible is 15, so each bank will add at most 1 to each of
-/// the banks that come after it.
-const SPREAD: [usize; 16] = [
+const REMOVE: u64 = 0x0fffffffffffffff;
+/// The highest number of banks when overflow is not a concern is 15, so each bank will
+/// add at most 1 to each of the banks that come after it.
+const SPREAD: [u64; 16] = [
     0x0000000000000000,
     0x0100000000000000,
     0x0110000000000000,
@@ -40,7 +44,7 @@ const SPREAD: [usize; 16] = [
 
 pub fn parse(input: &str) -> Input {
     // Accumulate the input into a single `u64`.
-    let mut memory: usize = input.iter_unsigned::<usize>().fold(0, |acc, n| (acc << 4) + n);
+    let mut memory: u64 = input.iter_unsigned::<u64>().fold(0, |acc, n| (acc << 4) + n);
     // Store previously seen configurations for cycle detection.
     let mut seen = FastMap::with_capacity(20_000);
     let mut cycles = 0;
@@ -63,15 +67,43 @@ pub fn parse(input: &str) -> Input {
         // The mask will have a 1 bit set for each of the joint highest values.
         // Choose the lowest index which is the most significant bit set.
         let offset = mask.leading_zeros();
-        let max = memory.rotate_left(offset + 4) & 0xf;
+        let max = (memory.rotate_left(offset + 4) & 0xf) as usize;
 
-        // Empty the largest memory bank and reallocate its contents to the following banks.
-        memory = (memory & REMOVE.rotate_right(offset)) + SPREAD[max].rotate_right(offset);
-        cycles += 1;
+        // Common case: no overflow
+        if max < 15 || mask.count_ones() == 1 {
+            // Empty the largest memory bank and reallocate its contents to the following banks.
+            memory = (memory & REMOVE.rotate_right(offset)) + SPREAD[max].rotate_right(offset);
+            cycles += 1;
 
-        // Check if we've seen this configuration before
-        if let Some(previous) = seen.insert(memory, cycles) {
-            break (cycles, cycles - previous);
+            // Check if we've seen this configuration before
+            if let Some(previous) = seen.insert(memory, cycles) {
+                break (cycles, cycles - previous);
+            }
+        } else {
+            // Overflow case.  This can happen in the early steps of the system, but resolves
+            // fairly quickly - in practice, the worst known input file had a total of 10 overflow
+            // cycles, with at most two adjacent overflows per encounter, with all overflows
+            // before cycle 200, well before the first repeated configuration.  Thus, it is
+            // okay to not cache these states in seen.
+
+            let mut array = [0; 16];
+
+            for i in (0..16).rev() {
+                array[i] = memory & 0xf;
+                memory >>= 4;
+            }
+
+            while array.iter().any(|&n| n >= 15) {
+                let max = *array.iter().max().unwrap();
+                let first = array.iter().position(|&n| n == max).unwrap();
+
+                array[first] = 0;
+                (0..max as usize).for_each(|i| array[(first + i + 1) % 16] += 1);
+
+                cycles += 1;
+            }
+
+            memory = array.iter().fold(0, |acc, n| (acc << 4) | n);
         }
     }
 }
