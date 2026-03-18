22302c4c8c7f8a4bdcd2b292d0d46de130fa860a
Eric Blake <eblake@redhat.com>
2026-03-18 16:21:08 -0500

    Experiment with 2x3 blocking instead of 2x2
    Reduces the number of iterations from 5 million to 4.1 million, but
    requires 10x the cache. This is enough memory that it overflows L1
    cache on my machine, and therefore ends up slower rather than faster
    due to memory contention (slows from 23ms to 30ms).  Submitting it to
    document a failed experiment, rather than as something to be applied.

--- src/year2017/day22.rs
+++ src/year2017/day22.rs
@@ -40,9 +40,13 @@ use crate::util::point::*;
 use std::array::from_fn;
 use std::mem::take;
 
+/// For part 1, `SIZE` is half the grid width or height, in order to start in the center.
+/// For part 2, `SIZE` is the number of 2x3 blocks in a row; a grid of 250x166 blocks can hold
+/// a range of 500x498 cells in 2x3 blocks. `CENTER` only matters in part 2.
 const SIZE: usize = 250;
-const HALF: usize = SIZE / 2;
-const CENTER: usize = SIZE * HALF + HALF;
+const HALF_WIDTH: usize = SIZE / 2;
+const THIRD_HEIGHT: usize = SIZE / 3;
+const CENTER: usize = HALF_WIDTH + SIZE * THIRD_HEIGHT;
 
 pub fn parse(input: &str) -> Grid<u8> {
     Grid::parse(input)
@@ -83,63 +87,72 @@ pub fn part1(input: &Grid<u8>) -> u32 {
     infected
 }
 
-/// Use a compressed grid where each byte stores 4 cells (2x2 block) with 2 bits per cell.
+/// Use a compressed grid where each byte stores 6 cells (2x3 block) with 2 bits per cell.
 pub fn part2(input: &Grid<u8>) -> usize {
     // Assume that the carrier will never go outside the range 0 to 500 in both x and y axes
-    // starting at the center. As we store 4 nodes per byte, we compress the x and y axes by two.
-    let mut grid = vec![0; SIZE * SIZE];
+    // starting at the center. As we store 6 nodes per u16, we compress the x axis by
+    // 2 and the y axis by 3, for a grid of 250 by 166 blocks.  Offset so that we start on
+    // heading 6 (moving up at sextant 4).
+    //
+    // The map of headings to u16 shift locations:
+    //    [0][1]
+    // [9] 0  1 [2]
+    // [8] 2  3 [3]
+    // [7] 4  5 [4]
+    //    [6][5]
+    let mut grid3 = vec![0; 2 * HALF_WIDTH * 2 * THIRD_HEIGHT];
 
-    // Precompute all 4 * 4 * 256 possible state transitions for faster simulation.
-    let cache: [[[_; 256]; 4]; 4] = from_fn(|quadrant| {
-        from_fn(|direction| from_fn(|state| compute_block(&mut grid, quadrant, direction, state)))
-    });
+    // Precompute all 10 * 4096 possible state transitions for faster simulation.
+    let cache3: [[_; 4096]; 10] =
+        from_fn(|heading| from_fn(|state| compute_block3(&mut grid3, heading, state)));
 
-    // Copy the smaller initial input grid to the center of the larger grid,
-    // packing 4 nodes into each byte.
-    let offset = SIZE - (input.width / 2) as usize;
+    // Copy the smaller initial input grid to the center of the larger grid, packing 6 nodes
+    // into each byte. Ensure that center,center maps to sextant 4 (ie. lines center-2, center-1,
+    // and center land in same u16), by putting the center at 250,251.
+    let offset = SIZE - (input.width as usize / 2);
 
     for y in 0..input.height {
         for x in 0..input.width {
             if input[Point::new(x, y)] == b'#' {
-                let (adjusted_x, adjusted_y) = (x as usize + offset, y as usize + offset);
-                let index = SIZE * (adjusted_y / 2) + (adjusted_x / 2);
-                let offset = 4 * (adjusted_y % 2) + 2 * (adjusted_x % 2);
+                let (adjusted_x, adjusted_y) = (x as usize + offset, y as usize + offset + 1);
+                let index = SIZE * (adjusted_y / 3) + (adjusted_x / 2);
+                let offset = 4 * (adjusted_y % 3) + 2 * (adjusted_x % 2);
                 // Mark node as infected.
-                grid[index] |= 2 << offset;
+                grid3[index] |= 2 << offset;
             }
         }
     }
 
-    // Start in the center of the grid, in the top-left corner of a 2x2 cell, facing up.
+    // Start in the center of the grid, in the bottom-left corner of a 2x3 cell, facing up.
     let mut index = CENTER;
-    let mut quadrant = 0; // Top-left corner
-    let mut direction = 0; // Facing up
+    let mut heading = 6; // Bottom-left corner, facing up
     let mut infected = 0;
     let mut remaining = 10_000_000;
 
-    // Memoized blocks can combine up to 8 steps. Handle the last few steps individually to
+    // Memoized blocks can combine up to 12 steps. Handle the last few steps individually to
     // prevent overshooting the step target and overcounting the infected node transitions.
-    while remaining > 8 {
-        let state = grid[index] as usize;
-        let packed = cache[quadrant][direction][state];
+    while remaining > 12 {
+        let state = grid3[index] as usize;
+        let packed = cache3[heading][state];
 
-        // With 10 million repetitions, saving time inside this hot loop is essential.
-        // By bit-packing 6 fields into a single `u32`, we limit the size of the array to 16kB
-        // making sure that it fits into L1 cache.
-        grid[index] = packed as u8; // bits 0-7
-        index = index + (packed >> 20) as usize - SIZE; // bits 20-31
-        quadrant = ((packed >> 8) % 4) as usize; // bits 8-9
-        direction = ((packed >> 10) % 4) as usize; // bits 10-11
-        infected += ((packed >> 12) % 16) as usize; // bits 12-15
-        remaining -= ((packed >> 16) % 16) as usize; // bits 16-19
+        // With 4.1 million cache lookups for 10 million repetitions, saving time inside this
+        // hot loop is essential. By bit-packing 5 fields into a single `u32`, we limit the size
+        // of the array to 160kB making sure that it is not too much larger than L1 cache.
+        grid3[index] = (packed % 4096) as u16; // bits 0-11
+        index = index + (packed >> 23) as usize - SIZE; // bits 23-31
+        heading = ((packed >> 12) % 16) as usize; // bits 12-15
+        infected += ((packed >> 16) % 8) as usize; // bits 16-18
+        remaining -= ((packed >> 19) % 16) as usize; // bits 19-22
     }
 
-    // Handle up to 8 remaining steps individually to prevent overcounting.
+    let (mut sextant, mut direction) = decode(heading);
+
+    // Handle up to 12 remaining steps individually to prevent overcounting.
     for _ in 0..remaining {
-        let [next_index, next_quadrant, next_direction, next_infected] =
-            step(&mut grid, index, quadrant, direction);
+        let [next_index, next_sextant, next_direction, next_infected] =
+            step3(&mut grid3, index, sextant, direction);
         index = next_index;
-        quadrant = next_quadrant;
+        sextant = next_sextant;
         direction = next_direction;
         infected += next_infected;
     }
@@ -147,22 +160,25 @@ pub fn part2(input: &Grid<u8>) -> usize {
     infected
 }
 
-/// Computes the number of steps taken, infected nodes and next location for 2 x 2 blocks of nodes.
+/// Computes the number of steps taken, infected nodes and next location for 2 x 3 blocks of nodes.
 #[inline]
-fn compute_block(grid: &mut [u8], mut quadrant: usize, mut direction: usize, state: usize) -> u32 {
+fn compute_block3(grid3: &mut [u16], heading: usize, state: usize) -> u32 {
     let mut index = CENTER;
     let mut infected = 0;
     let mut steps = 0;
 
     // Temporarily use the grid. This allows the index to move without exceeding bounds.
-    grid[CENTER] = state as u8;
+    grid3[CENTER] = state as u16;
+
+    // Convert heading into placement and direction
+    let (mut placement, mut direction) = decode(heading);
 
     // Count steps and infected nodes until we leave this cell.
     while index == CENTER {
-        let [next_index, next_quadrant, next_direction, next_infected] =
-            step(grid, index, quadrant, direction);
+        let [next_index, next_placement, next_direction, next_infected] =
+            step3(grid3, index, placement, direction);
         index = next_index;
-        quadrant = next_quadrant;
+        placement = next_placement;
         direction = next_direction;
         infected += next_infected;
         steps += 1;
@@ -170,26 +186,39 @@ fn compute_block(grid: &mut [u8], mut quadrant: usize, mut direction: usize, sta
 
     // Reset the grid to zero and figure out the next index. We offset index by SIZE to keep the
     // value positive for easier bit manipulation.
-    let next_state = take(&mut grid[CENTER]);
+    let next_state = take(&mut grid3[CENTER]);
     let next_index = index + SIZE - CENTER;
 
+    let heading = match (placement, direction) {
+        (0, 2) => 0,
+        (1, 2) => 1,
+        (1, 3) => 2,
+        (3, 3) => 3,
+        (5, 3) => 4,
+        (5, 0) => 5,
+        (4, 0) => 6,
+        (4, 1) => 7,
+        (2, 1) => 8,
+        _ => 9,
+    };
+
     // Pack six fields into a single `u32`, maximizing cache locality by minimizing space.
-    next_state as u32
-        | (quadrant << 8) as u32
-        | (direction << 10) as u32
-        | (infected << 12) as u32
-        | (steps << 16)
-        | (next_index << 20) as u32
+    next_state as u32  // 0-11
+        | (heading << 12) // 12-15 (value 0-9)
+        | (infected << 16) as u32 // 16-18 (value 0-4)
+        | (steps << 19) // 19-22 (value 0-12)
+        | (next_index << 23) as u32 // 23-31 (value SIZE-1,SIZE+1,SIZE-250,SIZE+250)
 }
 
 /// Process a single step in any arbitrary location on the grid.
 #[inline]
-fn step(grid: &mut [u8], index: usize, quadrant: usize, direction: usize) -> [usize; 4] {
-    // 4 nodes are packed into a single byte with quadrants arranged as:
+fn step3(grid3: &mut [u16], index: usize, placement: usize, direction: usize) -> [usize; 4] {
+    // 6 nodes are packed into a single byte with headings arranged as:
     // [ 0 1 ]
     // [ 2 3 ]
-    let shift = 2 * quadrant;
-    let node = (grid[index] >> shift) % 4;
+    // [ 4 5 ]
+    let shift = 2 * placement;
+    let node = (grid3[index] >> shift) % 4;
 
     // Nodes cycle between 4 possible values:
     // 0 = Clean, 1 = Weakened, 2 = Infected, 3 = Flagged
@@ -200,11 +229,11 @@ fn step(grid: &mut [u8], index: usize, quadrant: usize, direction: usize) -> [us
 
     // Update the 2 bits representing the current node.
     let mask = !(0b11 << shift);
-    grid[index] = (grid[index] & mask) | (next_node << shift);
+    grid3[index] = (grid3[index] & mask) | (next_node << shift);
 
     // Calculate x and y coordinates as if a single node was stored in each cell.
     // This is used in the next step in order to calculate if the index has changed.
-    let (x, y) = (2 * (index % SIZE) + quadrant % 2, 2 * (index / SIZE) + quadrant / 2);
+    let (x, y) = (2 * (index % SIZE) + placement % 2, 3 * (index / SIZE) + placement / 2);
     let (x, y) = match next_direction {
         0 => (x, y - 1),
         1 => (x + 1, y),
@@ -212,10 +241,26 @@ fn step(grid: &mut [u8], index: usize, quadrant: usize, direction: usize) -> [us
         _ => (x - 1, y),
     };
 
-    // Convert the x and y coordinates back into the compressed values for 2 x 2 nodes in each cell.
-    let next_index = SIZE * (y / 2) + (x / 2);
-    let next_quadrant = 2 * (y % 2) + (x % 2);
+    // Convert the x and y coordinates back into the compressed values for 2 x 3 nodes in each cell.
+    let next_index = SIZE * (y / 3) + (x / 2);
+    let next_placement = 2 * (y % 3) + (x % 2);
     let infected = usize::from(next_node == 2);
 
-    [next_index, next_quadrant, next_direction, infected]
+    [next_index, next_placement, next_direction, infected]
+}
+
+/// Map a heading (0-9) into its sextant and direction.
+fn decode(heading: usize) -> (usize, usize) {
+    match heading {
+        0 => (0, 2),
+        1 => (1, 2),
+        2 => (1, 3),
+        3 => (3, 3),
+        4 => (5, 3),
+        5 => (5, 0),
+        6 => (4, 0),
+        7 => (4, 1),
+        8 => (2, 1),
+        _ => (0, 1),
+    }
 }
