9354c02844fce4764087f00504aff4264b0980bd
Eric Blake <32933908+ebblake@users.noreply.github.com>
2026-03-30 08:54:41 -0500

    Add unit test coverage for recent bug fix (#63)
    Set up an input file that triggers the issues that were present before
    the bug fix in commit 43d9a6ea8e.  In particular, flowing water
    spreading until it is adjacent to another column that also has flowing
    water, as well as a one-column gap at the end of one barrier being
    adjacent to unconnected clay.

--- tests/year2018/day17.rs
+++ tests/year2018/day17.rs
@@ -10,14 +10,28 @@ x=498, y=10..13
 x=504, y=10..13
 y=13, x=498..504";
 
+/// Some additional corner cases worth getting correct. This input produces a spread that ends
+/// adjacent to another column that already contains Moving water, as well as finding a one-column
+/// gap adjacent to unconnected clay that should still result in Moving water.
+const EXTRA: &str = "\
+x=500, y=2..3
+x=501, y=5..6
+y=7, x=501..502
+x=504, y=1..6
+y=9, x=503..504";
+
 #[test]
 fn part1_test() {
     let input = parse(EXAMPLE);
     assert_eq!(part1(&input), 57);
+    let input = parse(EXTRA);
+    assert_eq!(part1(&input), 31);
 }
 
 #[test]
 fn part2_test() {
     let input = parse(EXAMPLE);
     assert_eq!(part2(&input), 29);
+    let input = parse(EXTRA);
+    assert_eq!(part2(&input), 0);
 }
