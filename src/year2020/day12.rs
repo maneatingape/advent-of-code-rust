55b154a0390dda4ad46f17c498a1135fb66b04c8
Terje Wiig Mathisen <terje.mathisen@cognite.com>
2026-06-14 16:36:06 +0200

    fmt+clippy updates

--- src/year2020/day12.rs
+++ src/year2020/day12.rs
@@ -4,59 +4,78 @@
 //! a custom parser solves both parts during a single pass over the input bytes
 //!
 
-type Input = (i32,i32);
+type Input = (i32, i32);
 
-const DX:[i32;4] = [1,0,-1,0];
-const DY:[i32;4] = [0,1,0,-1];
+const DX: [i32; 4] = [1, 0, -1, 0];
+const DY: [i32; 4] = [0, 1, 0, -1];
 
 pub fn parse(input: &str) -> Input {
     let mut inp = input.to_string();
-    if inp.as_bytes()[inp.len()-1] != b'\n' { inp.push('\n');}
+    if inp.as_bytes()[inp.len() - 1] != b'\n' {
+        inp.push('\n');
+    }
     let bytes = inp.as_bytes();
     let mut i = 0;
-    let mut x:i32 = 0;
-    let mut y:i32 = 0;
-    let mut direction:usize = 0;
-    let mut x2:i32 = 0;
-    let mut y2:i32 = 0;
-    let mut wx:i32 = 10; // Waypoint starts at E10, N1
-    let mut wy:i32 = -1;
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
+    let mut direction: usize = 0;
+    let mut x2: i32 = 0;
+    let mut y2: i32 = 0;
+    let mut wx: i32 = 10; // Waypoint starts at E10, N1
+    let mut wy: i32 = -1;
 
     while i < bytes.len() {
-        let cmd = bytes[i]; i+= 1;
-        let mut n:i32 = (bytes[i] - b'0') as i32; i += 1;
+        let cmd = bytes[i];
+        i += 1;
+        let mut n: i32 = (bytes[i] - b'0') as i32;
+        i += 1;
         while bytes[i] != b'\n' {
-            n = n*10 + (bytes[i] - b'0') as i32; i += 1;
+            n = n * 10 + (bytes[i] - b'0') as i32;
+            i += 1;
         }
-        i += 1;  // skip the newline
+        i += 1; // skip the newline
         match cmd {
-            b'N' => {y -= n; wy -= n;},
-            b'S' => {y += n; wy += n;},
-            b'E' => {x += n; wx += n;},
-            b'W' => {x -= n; wx -= n;},
+            b'N' => {
+                y -= n;
+                wy -= n;
+            }
+            b'S' => {
+                y += n;
+                wy += n;
+            }
+            b'E' => {
+                x += n;
+                wx += n;
+            }
+            b'W' => {
+                x -= n;
+                wx -= n;
+            }
             b'L' => {
-                let mut ddir = n/90; 
+                let mut ddir = n / 90;
                 direction = (direction + 4 - ddir as usize) & 3;
                 while ddir > 0 {
-                    (wx,wy) = (wy,-wx);
+                    (wx, wy) = (wy, -wx);
                     ddir -= 1;
                 }
-            },
+            }
             b'R' => {
-                let mut ddir = n/90; 
+                let mut ddir = n / 90;
                 direction = (direction + ddir as usize) & 3;
                 while ddir > 0 {
-                    (wx,wy) = (-wy,wx);
+                    (wx, wy) = (-wy, wx);
                     ddir -= 1;
                 }
-            },
+            }
             b'F' => {
-                x += DX[direction]*n; 
-                y += DY[direction]*n;
-                x2 += wx*n;
-                y2 += wy*n;
-            },
-            _ => {panic!("Bad command {cmd}")},
+                x += DX[direction] * n;
+                y += DY[direction] * n;
+                x2 += wx * n;
+                y2 += wy * n;
+            }
+            _ => {
+                panic!("Bad command {cmd}")
+            }
         }
     }
     let part1 = x.abs() + y.abs();
