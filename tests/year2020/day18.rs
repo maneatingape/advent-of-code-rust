23aa6caa1a91ea4545565dde2b6a1e51f3b6f06f
Eric Blake <eblake@redhat.com>
2026-06-29 16:51:28 -0500

    Handle newlines more efficiently
    Calling iter.lines() to separate input into lines, only to then join
    things up later, is less efficient than just handling newlines
    specially as part of the overall parse.  The easiest way to parse the
    entire file quickly is to treat each line as an implicit () expression
    joined by +, and the easiest way to change '\n' in the input into
    ')+(' that the rest of the code parses is with a custom iterator
    wrapper.
    
    On my laptop, this cuts the overall day from 71us to 66us.

--- tests/year2020/day18.rs
+++ tests/year2020/day18.rs
@@ -6,16 +6,17 @@ const EXAMPLE: &str = "\
 2 * 3 + (4 * 5)
 5 + (8 * 3 + 9 + 3 * 4 * 3)
 5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))
-((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2";
+((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2
+";
 
 #[test]
 fn part1_test() {
     let input = parse(EXAMPLE);
-    assert_eq!(part1(&input), 26457);
+    assert_eq!(part1(input), 26457);
 }
 
 #[test]
 fn part2_test() {
     let input = parse(EXAMPLE);
-    assert_eq!(part2(&input), 694173);
+    assert_eq!(part2(input), 694173);
 }
