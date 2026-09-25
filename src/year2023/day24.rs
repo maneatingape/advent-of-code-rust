d70b323d0db2fdcb289369a0e3d3fc167de81be5
Eric Blake <eblake@redhat.com>
2026-09-24 20:39:40 -0500

    Failed experiment with Bentley-Ottmann algorithm
    https://en.wikipedia.org/wiki/Bentley%E2%80%93Ottmann_algorithm
    
    On paper, O((n+k)log n) has a chance at beating O(n^2), if k is small
    enough.  In practice, my input had k > 22000, or nearly 60% of n^2;
    and sticking to 44850 pairwise comparisons at O(1) each is faster than
    performing 22000+ manipulations of both a MinHeap and BTree, each with
    O(log n) bookkeeping overhead.  Furthermore, the actual
    Bentley-Ottmann algorithm assumes a MinHeap that can unqueue arbitrary
    entries, which matters at keeping the tree strictly bounded to at most
    3n items; while I just blindly left all intersections laying around
    which means the priority queue can get quite big; there's a sizeable
    overhead difference between log 300 and log 22000.  Runtime exploded
    from 900us to 30ms.  But hey, it was fun trying.

--- src/year2023/day24.rs
+++ src/year2023/day24.rs
@@ -53,13 +53,175 @@
 //! Given the rock direction we can calculate the times that the two hailstones are intercepted
 //! then use this to determine the original position of the rock, as long as the two times
 //! are different.
-use std::ops::{Add, RangeInclusive, Sub};
+use crate::util::hash::*;
+use crate::util::heap::*;
+use std::cell::Cell;
+use std::cmp::Ordering;
+use std::collections::BTreeSet;
+use std::ops::{Add, Sub};
+use std::rc::Rc;
 
 use crate::util::iter::*;
 use crate::util::math::*;
 use crate::util::parse::*;
 
-const RANGE: RangeInclusive<i64> = 200_000_000_000_000..=400_000_000_000_000;
+const LOW: i128 = 200_000_000_000_000;
+const HIGH: i128 = 400_000_000_000_000;
+const SCALE_BITS: u32 = 32;
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+enum EventType {
+    Start,
+    Intersection,
+    End,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct LineSegment {
+    id: usize,
+    m: i128,       // Scaled by SCALE_BITS.
+    b: i128,       // Scaled by SCALED_BITS.
+    x_start: i128, // Scaled by SCALED_BITS.
+    x_end: i128,   // Scaled by SCALED_BITS.
+}
+
+impl LineSegment {
+    fn y_at(&self, x: i128) -> i128 {
+        ((self.m * x) >> SCALE_BITS) + self.b
+    }
+}
+
+#[derive(Clone)]
+struct StatusKey {
+    segment: LineSegment,
+    current_x: Rc<Cell<i128>>,
+}
+
+impl PartialEq for StatusKey {
+    fn eq(&self, other: &Self) -> bool {
+        self.segment.id == other.segment.id
+    }
+}
+
+impl Eq for StatusKey {}
+
+impl Ord for StatusKey {
+    // Even though the y value of the key changes at runtime as current_x sweeps, the algorithm
+    // guarantees that any two entries in the table have the same relative ordering for as long as
+    // the key remains in the table.
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.segment.id == other.segment.id {
+            return Ordering::Equal;
+        }
+        let x = self.current_x.get();
+        let y1 = self.segment.y_at(x);
+        let y2 = other.segment.y_at(x);
+
+        // Primary sort: y positions anywhere along the range of current_x.
+        let mut ord = y1.cmp(&y2);
+
+        // Tie-breaker at intersections: compare current_x use slope of faster-growing id.
+        if ord == Ordering::Equal {
+            let intersect_x = find_segment_intersection(&self.segment, &other.segment).unwrap();
+            if x < intersect_x {
+                ord = other.segment.m.cmp(&self.segment.m);
+            } else {
+                ord = self.segment.m.cmp(&other.segment.m);
+            }
+        }
+
+        // Fallback tie-breaker.
+        if ord == Ordering::Equal {
+            ord = self.segment.id.cmp(&other.segment.id);
+        }
+
+        ord
+    }
+}
+
+impl PartialOrd for StatusKey {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[derive(Clone, Eq)]
+struct SweepEvent {
+    x: i128, // Scaled by SCALED_BITS.
+    event: EventType,
+    y: i128, // Scaled by SCALED_BITS.
+    id: usize,
+    other: Option<usize>, // only for Intersection
+}
+
+impl Ord for SweepEvent {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.x
+            .cmp(&other.x)
+            .then_with(|| self.event.cmp(&other.event))
+            .then_with(|| self.y.cmp(&other.y))
+            .then_with(|| self.id.cmp(&other.id))
+    }
+}
+
+impl PartialOrd for SweepEvent {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for SweepEvent {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.event == other.event && self.y == other.y && self.id == other.id
+    }
+}
+
+struct SweepStatus {
+    tree: BTreeSet<StatusKey>,
+    current_x: Rc<Cell<i128>>,
+}
+
+impl SweepStatus {
+    fn new(current_x: Rc<Cell<i128>>) -> Self {
+        Self { tree: BTreeSet::new(), current_x }
+    }
+
+    fn get_neighbors(&self, key: &StatusKey) -> (Option<LineSegment>, Option<LineSegment>) {
+        let above = self.tree.range(key..).nth(1).map(|k| k.segment);
+        let below = self.tree.range(..key).next_back().map(|k| k.segment);
+        (above, below)
+    }
+
+    fn swap_segments(
+        &mut self,
+        seg1: LineSegment,
+        seg2: LineSegment,
+    ) -> (Option<LineSegment>, Option<LineSegment>) {
+        let key1 = StatusKey { segment: seg1, current_x: Rc::clone(&self.current_x) };
+        let key2 = StatusKey { segment: seg2, current_x: Rc::clone(&self.current_x) };
+        // Tweak the clock to ensure cmp() still sees old order for both segments.
+        let old_x = self.current_x.get();
+        self.current_x.set(old_x - 1);
+
+        // Sanity check before deleting the old values.
+        let (above, below) = self.get_neighbors(&key1);
+        assert!(above.is_some_and(|s| s.id == seg2.id) || below.is_some_and(|s| s.id == seg2.id));
+        let removed1 = self.tree.remove(&key1);
+        let removed2 = self.tree.remove(&key2);
+        assert!(removed1 && removed2);
+
+        // Another time tweak before re-inserting.
+        self.current_x.set(old_x + 1);
+        self.tree.insert(key1.clone());
+        self.tree.insert(key2.clone());
+        let (ab1, bl1) = self.get_neighbors(&key1);
+        let (ab2, bl2) = self.get_neighbors(&key2);
+
+        // Now the accurate time and correct neighbors.
+        self.current_x.set(old_x);
+        if ab1.is_some_and(|s| s.id == seg2.id) { (ab2, bl1) } else { (ab1, bl2) }
+    }
+}
 
 #[derive(Clone, Copy)]
 struct Vector {
@@ -108,34 +270,103 @@ pub fn parse(input: &str) -> Vec<[i64; 6]> {
     input.iter_signed().chunk::<6>().collect()
 }
 
-pub fn part1(input: &[[i64; 6]]) -> u32 {
-    let mut result = 0;
+pub fn part1(input: &[[i64; 6]]) -> usize {
+    // Shared sweep-line clock, initialized below LOW.
+    let current_x = Rc::new(Cell::new(0_i128));
+    let mut segments = Vec::with_capacity(input.len());
 
-    for (index, &[a, b, _, c, d, _]) in input.iter().enumerate() {
-        for &[e, f, _, g, h, _] in &input[..index] {
-            // If the determinant is zero there is no solution possible
-            // which implies the trajectories are parallel.
-            let determinant = d * g - c * h;
-            if determinant == 0 {
-                continue;
+    // Input files have around 60% of the n² intersections within bounds; but n is small enough that
+    // oversizing this set does not hurt.
+    let mut seen = FastSet::with_capacity(30_000);
+
+    // The canonical Bentley-Ottman algorithm never has more than 3n events in the queue at once,
+    // but that requires the ability to remove intersection events when segments are swapped to no
+    // longer be adjacent in scan_line. Easier is to just allow the queue to be as large as the
+    // expected output, even though that is oversized.
+    let mut todo = MinHeap::with_capacity(seen.capacity());
+    let mut status = SweepStatus::new(Rc::clone(&current_x));
+
+    // For each point that intersects the bounding box, insert start and end events.
+    for &[a, b, _, c, d, _] in input {
+        if let Some(seg) = clip_to_bounds(segments.len(), a, b, c, d) {
+            segments.push(seg);
+            todo.push(
+                SweepEvent {
+                    x: seg.x_start,
+                    event: EventType::Start,
+                    y: seg.y_at(seg.x_start),
+                    id: seg.id,
+                    other: None,
+                },
+                (),
+            );
+            todo.push(
+                SweepEvent {
+                    x: seg.x_end,
+                    event: EventType::End,
+                    y: seg.y_at(seg.x_end),
+                    id: seg.id,
+                    other: None,
+                },
+                (),
+            );
+        }
+    }
+
+    // For each action in the work queue, update the scan_line order of currently-active segments,
+    // and queue up any intersections revealed by adjacent segments.
+    while let Some((event, ())) = todo.pop() {
+        current_x.set(event.x);
+        match event.event {
+            EventType::Start => {
+                let seg = segments[event.id];
+                let key = StatusKey { segment: seg, current_x: Rc::clone(&current_x) };
+                status.tree.insert(key.clone());
+
+                let (above, below) = status.get_neighbors(&key);
+                if let Some(ab) = above {
+                    check_intersect(&seg, &ab, &mut todo, &mut seen, current_x.get());
+                }
+                if let Some(bl) = below {
+                    check_intersect(&seg, &bl, &mut todo, &mut seen, current_x.get());
+                }
             }
 
-            // Invert the 2x2 matrix then multiply by the respective columns to find the times.
-            let t = (g * (f - b) - h * (e - a)) / determinant;
-            let u = (c * (f - b) - d * (e - a)) / determinant;
+            EventType::End => {
+                let seg = segments[event.id];
+                let key = StatusKey { segment: seg, current_x: Rc::clone(&current_x) };
+                let (above, below) = status.get_neighbors(&key);
+                let removed = status.tree.remove(&key);
+                assert!(removed);
+                if let Some(ab) = above
+                    && let Some(bl) = below
+                {
+                    check_intersect(&ab, &bl, &mut todo, &mut seen, current_x.get());
+                }
+            }
 
-            // We can pick either the first or second hailstone to find the intersection position.
-            let x = a + t * c;
-            let y = b + t * d;
+            EventType::Intersection => {
+                let id1 = event.id;
+                let id2 = event.other.unwrap();
+                let seg1 = segments[id1];
+                let seg2 = segments[id2];
 
-            // Both times must be in the future and the position within the specified area.
-            if t >= 0 && u >= 0 && RANGE.contains(&x) && RANGE.contains(&y) {
-                result += 1;
+                // The algorithm says the two must be neighbors, but not which one was first.
+                let (above, below) = status.swap_segments(seg1, seg2);
+                // It is okay if check_intersect is called more than necessary.
+                if let Some(ab) = above {
+                    check_intersect(&seg1, &ab, &mut todo, &mut seen, current_x.get());
+                    check_intersect(&seg2, &ab, &mut todo, &mut seen, current_x.get());
+                }
+                if let Some(bl) = below {
+                    check_intersect(&seg1, &bl, &mut todo, &mut seen, current_x.get());
+                    check_intersect(&seg2, &bl, &mut todo, &mut seen, current_x.get());
+                }
             }
         }
     }
 
-    result
+    seen.len()
 }
 
 pub fn part2(input: &[[i64; 6]]) -> i128 {
@@ -179,4 +410,83 @@ pub fn part2(input: &[[i64; 6]]) -> i128 {
     let b = (p0 + p4).sum();
     let c = (v3 - v4).sum();
     (u * a - t * b + u * t * c) / (u - t)
+}
+
+fn clip_to_bounds(id: usize, x: i64, y: i64, dx: i64, dy: i64) -> Option<LineSegment> {
+    let x0 = x as i128;
+    let y0 = y as i128;
+    let vx = dx as i128;
+    let vy = dy as i128;
+    let (tx_min, tx_max) = if dx > 0 {
+        (((LOW - x0) << SCALE_BITS) / vx, ((HIGH - x0) << SCALE_BITS) / vx)
+    } else {
+        (((HIGH - x0) << SCALE_BITS) / vx, ((LOW - x0) << SCALE_BITS) / vx)
+    };
+
+    let (ty_min, ty_max) = if dy > 0 {
+        (((LOW - y0) << SCALE_BITS) / vy, ((HIGH - y0) << SCALE_BITS) / vy)
+    } else {
+        (((HIGH - y0) << SCALE_BITS) / vy, ((LOW - y0) << SCALE_BITS) / vy)
+    };
+
+    let t_start = tx_min.max(ty_min).max(0);
+    let t_end = tx_max.min(ty_max);
+    if t_start >= t_end {
+        return None;
+    }
+
+    // Scale to fixed-point domain.
+    let mut x_start = (x0 << SCALE_BITS) + (t_start * vx);
+    let mut x_end = (x0 << SCALE_BITS) + (t_end * vx);
+
+    let m = (vy << SCALE_BITS) / vx;
+    let b = (y0 << SCALE_BITS) - (m * x0);
+
+    if x_start > x_end {
+        (x_start, x_end) = (x_end, x_start);
+    }
+
+    Some(LineSegment { id, m, b, x_start, x_end })
+}
+
+fn check_intersect(
+    seg1: &LineSegment,
+    seg2: &LineSegment,
+    todo: &mut MinHeap<SweepEvent, ()>,
+    seen: &mut FastSet<(usize, usize)>,
+    now: i128,
+) {
+    let pair_key = if seg1.id < seg2.id { (seg1.id, seg2.id) } else { (seg2.id, seg1.id) };
+    if seen.contains(&pair_key) {
+        return;
+    }
+    if let Some(ix) = find_segment_intersection(seg1, seg2)
+        && ix >= now
+    {
+        seen.insert(pair_key);
+        todo.push(
+            SweepEvent {
+                x: ix,
+                event: EventType::Intersection,
+                y: seg1.y_at(ix),
+                id: seg1.id,
+                other: Some(seg2.id),
+            },
+            (),
+        );
+    }
+}
+
+fn find_segment_intersection(seg1: &LineSegment, seg2: &LineSegment) -> Option<i128> {
+    if seg1.m == seg2.m {
+        return None; // Parallel lines.
+    }
+    let num = seg2.b - seg1.b;
+    let den = seg1.m - seg2.m;
+    let intersect_x = (num << SCALE_BITS) / den;
+    (intersect_x >= seg1.x_start
+        && intersect_x <= seg1.x_end
+        && intersect_x >= seg2.x_start
+        && intersect_x <= seg2.x_end)
+        .then_some(intersect_x)
 }
